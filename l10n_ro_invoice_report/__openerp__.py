de925db0d02102989784e0b26cecbc87c400b4f3
Dorin Hongu <dhongu@gmail.com>
2014-12-22 23:53:23 +0200

    Adaugare raport chitanta

--- l10n_ro_invoice_report/__openerp__.py
+++ l10n_ro_invoice_report/__openerp__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 # Copyright (c) 2008 Deltatech All Rights Reserved
-#                    Dorin Hongu <dhongu(@)gmail(.)com
+#                    Dorin Hongu <dhongu(@)gmail(.)com       
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -25,20 +25,28 @@
     "version" : "1.0",
     "author" : "Dorin Hongu",
     "website" : "",
-
+    
     "description": """
-
-Romania - Invoice Report layout
+    
+Romania - Invoice Report layout 
 ------------------------------------------
+ - factura contine: pretul fara tva, valoare si valoare tva
+
+La companie a fost adaugat un camp nou pentru capitalul social
+La factura au fost adugate campurile delegat si mijloc de transport
 
     """,
-
+    
     "category" : "Generic Modules",
     "depends" : ["base","account",'l10n_ro'],
 
 
     "data" : [
-                 'views/templates.xml',
+                 'views/invoice_report.xml',
+                 'views/voucher_report.xml',
+                 'company_view.xml',
+                 'account_invoice_view.xml',
+                 'account_voucher_report.xml'
                ],
     "active": False,
     "installable": True,
