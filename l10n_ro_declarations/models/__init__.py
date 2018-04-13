52017edd617d00aa4e279af0aeb210552a1953dc
Dorin Hongu <dhongu@gmail.com>
2018-04-13 20:14:56 +0300

    declaratii

---
+++ l10n_ro_declarations/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# ©  2018 Terrabit
+# See README.rst file on addons root folder for license details
+
+from . import account
+from . import declarations
+from . import d000
+from . import d100
+from . import d300
+from . import d394
+
