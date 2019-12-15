adb67234590570beb357bd6780983b35082729a9
Dorin Hongu <dhongu@gmail.com>
2019-12-15 08:26:56 +0200

    Eliminare camp si corectie afisare watermark_image

--- l10n_ro_invoice_report/models/account_invoice.py
+++ l10n_ro_invoice_report/models/account_invoice.py
@@ -1,25 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-# Copyright (c) 2008 Deltatech All Rights Reserved
-#                    Dorin Hongu <dhongu(@)gmail(.)com
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
-##############################################################################
-
+# ©  2008-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
 
 import odoo.addons.decimal_precision as dp
 
@@ -66,8 +48,9 @@ class account_invoice_line(models.Model):
     sequence = fields.Integer(default=1)
 
     # pretul fara tva utilizat pentru situatia in care tva este inclus in pret
-    price_unit_without_taxes = fields.Float(string='Unit Price without taxes', readonly=True,  # store=True,
-                                            compute='_compute_price')
+    # valoarea este caclulata direct in factura
+    # price_unit_without_taxes = fields.Float(string='Unit Price without taxes', readonly=True,  # store=True,
+    #                                         compute='_compute_price')
 
     # se va utiliza campul stndard price_tax
     #price_taxes = fields.Float(string='Taxes', digits=dp.get_precision('Account'), store=True, readonly=True,
@@ -92,52 +75,52 @@ class account_invoice_line(models.Model):
     price_tax = fields.Monetary(string='Tax Amount', compute='_get_price_tax', store=False)
     """
 
-    @api.one
-    @api.depends('price_unit', 'discount', 'invoice_line_tax_ids', 'quantity',
-        'product_id', 'invoice_id.partner_id', 'invoice_id.currency_id', 'invoice_id.company_id',
-        'invoice_id.date_invoice', 'invoice_id.date')
-    def _compute_price(self):
-
-        super(account_invoice_line, self)._compute_price()
-        if self.price_subtotal:
-            quantity = self.quantity or 1
-            self.price_unit_without_taxes = self.price_subtotal / quantity
-            #self.price_taxes = (self.price_total - self.price_subtotal) / quantity
-
-
-    def _compute_price_complex(self):
-        #Versiunea mai complexa
-        price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
-
-        currency = self.invoice_id and self.invoice_id.currency_id or None
-
-        taxes = False
-        if self.invoice_line_tax_ids:
-            taxes = self.invoice_line_tax_ids.compute_all(price, currency, quantity=self.quantity, product=self.product_id,
-                                                          partner=self.invoice_id.partner_id)
-
-        # de ce se seteaza cele doua valori ? nu au fost setate in
-        # if taxes:
-        #     self.price_subtotal = taxes['total_excluded'] if taxes else self.quantity * price
-        #     self.price_taxes = taxes['total_included'] - self.price_subtotal
-
-        taxes_unit = self.invoice_line_tax_ids.compute_all(price, currency=currency,
-                                                           quantity=1, product=self.product_id,
-                                                           partner=self.invoice_id.partner_id)
-
-        self.price_unit_without_taxes = taxes_unit['total_excluded']
-        # Compute normal taxes in case of Customer Invoices to have the value
-        # in Inverse Taxation
-        if self.invoice_id.type in ['out_invoice', 'out_refund']:
-            taxes_ids = self.product_id.taxes_id.filtered(lambda r: r.company_id == self.invoice_id.company_id)
-            normal_taxes = taxes_ids.compute_all(price, currency=currency,
-                                                                quantity=self.quantity, product=self.product_id,
-                                                                partner=self.invoice_id.partner_id)
-            self.price_normal_taxes = normal_taxes['total_included'] - normal_taxes['total_excluded']
-        # aplicare rotunjiri . asta nu trebuie facuta in functie de config
-        if self.invoice_id:
-            #self.price_subtotal = self.invoice_id.currency_id.round(self.price_subtotal)
-            #self.price_taxes = self.invoice_id.currency_id.round(self.price_taxes)
-            self.price_unit_without_taxes = self.invoice_id.currency_id.round(self.price_unit_without_taxes)
-            self.price_normal_taxes = self.invoice_id.currency_id.round(self.price_normal_taxes)
-
+    # @api.one
+    # @api.depends('price_unit', 'discount', 'invoice_line_tax_ids', 'quantity',
+    #     'product_id', 'invoice_id.partner_id', 'invoice_id.currency_id', 'invoice_id.company_id',
+    #     'invoice_id.date_invoice', 'invoice_id.date')
+    # def _compute_price(self):
+    #
+    #     super(account_invoice_line, self)._compute_price()
+    #     if self.price_subtotal:
+    #         quantity = self.quantity or 1
+    #         self.price_unit_without_taxes = self.price_subtotal / quantity
+    #         #self.price_taxes = (self.price_total - self.price_subtotal) / quantity
+    #
+    #
+    # def _compute_price_complex(self):
+    #     #Versiunea mai complexa
+    #     price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
+    #
+    #     currency = self.invoice_id and self.invoice_id.currency_id or None
+    #
+    #     taxes = False
+    #     if self.invoice_line_tax_ids:
+    #         taxes = self.invoice_line_tax_ids.compute_all(price, currency, quantity=self.quantity, product=self.product_id,
+    #                                                       partner=self.invoice_id.partner_id)
+    #
+    #     # de ce se seteaza cele doua valori ? nu au fost setate in
+    #     # if taxes:
+    #     #     self.price_subtotal = taxes['total_excluded'] if taxes else self.quantity * price
+    #     #     self.price_taxes = taxes['total_included'] - self.price_subtotal
+    #
+    #     taxes_unit = self.invoice_line_tax_ids.compute_all(price, currency=currency,
+    #                                                        quantity=1, product=self.product_id,
+    #                                                        partner=self.invoice_id.partner_id)
+    #
+    #     self.price_unit_without_taxes = taxes_unit['total_excluded']
+    #     # Compute normal taxes in case of Customer Invoices to have the value
+    #     # in Inverse Taxation
+    #     if self.invoice_id.type in ['out_invoice', 'out_refund']:
+    #         taxes_ids = self.product_id.taxes_id.filtered(lambda r: r.company_id == self.invoice_id.company_id)
+    #         normal_taxes = taxes_ids.compute_all(price, currency=currency,
+    #                                                             quantity=self.quantity, product=self.product_id,
+    #                                                             partner=self.invoice_id.partner_id)
+    #         self.price_normal_taxes = normal_taxes['total_included'] - normal_taxes['total_excluded']
+    #     # aplicare rotunjiri . asta nu trebuie facuta in functie de config
+    #     if self.invoice_id:
+    #         #self.price_subtotal = self.invoice_id.currency_id.round(self.price_subtotal)
+    #         #self.price_taxes = self.invoice_id.currency_id.round(self.price_taxes)
+    #         self.price_unit_without_taxes = self.invoice_id.currency_id.round(self.price_unit_without_taxes)
+    #         self.price_normal_taxes = self.invoice_id.currency_id.round(self.price_normal_taxes)
+    #
