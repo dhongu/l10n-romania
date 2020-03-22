955e24ba7dc5ec20335df83b95c780879e4e3945
Dorin Hongu <dhongu@gmail.com>
2020-03-22 17:39:45 +0200

    adaugare ecrane pt afisare note contabile in comanda de productie

---
+++ l10n_ro_mrp_account/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# ©  2008-2020 Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Romania - MRP Accounting",
+    "version": "1.0",
+    "author": "Terrabit",
+    "website": "http://www.terrabit.ro",
+    "category": "Warehouse",
+    "depends": ['l10n_ro_stock_account', 'mrp'],
+
+    'data': [
+    'views/mrp_view.xml'
+    ],
+
+}
