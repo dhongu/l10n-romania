d5c5583d9ddd7bfca30727d8972721eaa1fabde0
Dorin Hongu <dhongu@gmail.com>
2021-02-09 18:51:05 +0200

    pos

--- l10n_ro_pos/__manifest__.py
+++ l10n_ro_pos/__manifest__.py
@@ -1,9 +1,9 @@
-# ©  2015-2018 Deltatech
+# ©  2015-2021 Deltatech
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 {
     "name": "Romania - Point of Sale",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "category": "Localization",
     "license": "AGPL-3",
     "author": "Terrabit,Odoo Community Association (OCA)",
---
+++ l10n_ro_pos/models/pos_session.py
@@ -0,0 +1,37 @@
+# ©  2015-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from collections import defaultdict
+
+from odoo import models
+
+
+class PosSession(models.Model):
+    _inherit = "pos.session"
+
+    def _reconcile_account_move_lines(self, data):
+        if self.company_id.romanian_accounting:
+            data["stock_output_lines"] = {}
+        return super(PosSession, self)._reconcile_account_move_lines(data)
+
+    def _accumulate_amounts(self, data):
+        def get_amounts():
+            res = {"amount": 0.0, "amount_converted": 0.0}
+            return res
+
+        data = super(PosSession, self)._accumulate_amounts(data)
+
+        amounts = get_amounts
+
+        if self.company_id.romanian_accounting:
+            # nu trebuie generate note contabile  pentru ca acestea sunt generate in miscarea de stoc
+            data.update(
+                {
+                    "stock_expense": defaultdict(amounts),
+                    "stock_return": defaultdict(amounts),
+                    "stock_output": defaultdict(amounts),
+                }
+            )
+
+        return data
