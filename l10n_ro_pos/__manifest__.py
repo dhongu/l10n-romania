ca2ccd420624c50e52643eec84aa8598b861ae82
Dorin Hongu <dhongu@gmail.com>
2021-06-20 10:57:02 +0300

    adaugare de inregistrari in extrasul de banca

--- l10n_ro_pos/__manifest__.py
+++ l10n_ro_pos/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Terrabit,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "depends": ["point_of_sale", "l10n_ro_stock"],
-    "data": ["views/assets.xml"],
+    "data": ["views/assets.xml", "views/pos_payment_method_view.xml"],
     "qweb": ["static/src/xml/pos.xml"],
     "maintainers": ["dhongu"],
 }
