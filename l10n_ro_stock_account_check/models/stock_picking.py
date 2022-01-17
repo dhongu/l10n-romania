f48373f73f0767b272ee9c8c79a8d262e9751401
Dorin Hongu <dhongu@gmail.com>
2022-01-17 14:39:37 +0200

    verificare

---
+++ l10n_ro_stock_account_check/models/stock_picking.py
@@ -0,0 +1,13 @@
+# ©  2015-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    def correction_valuation(self):
+        for picking in self:
+            picking.move_lines.correction_valuation()
