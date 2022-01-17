54e45c94843571d8496a391472013150abed36b5
Dorin Hongu <dhongu@gmail.com>
2022-01-17 14:39:37 +0200

    verificare

--- l10n_ro_stock_account_check/__manifest__.py
+++ l10n_ro_stock_account_check/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Romania - Stock Accounting Check",
     "license": "AGPL-3",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "author": "Terrabit,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Warehouse",
@@ -16,6 +16,7 @@
         "report/stock_check_report_view.xml",
         "views/stock_valuation_layer_view.xml",
         "security/ir.model.access.csv",
+        "views/stock_view.xml",
     ],
     "installable": True,
     "maintainers": ["dhongu"],
