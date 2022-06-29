66e39abf059b7c273b025020721ffd446ff25b7f
Dorin Hongu <dhongu@gmail.com>
2022-06-29 17:46:33 +0300

    generare UBL

---
+++ l10n_ro_edi_ubl/models/__init__.py
@@ -0,0 +1,7 @@
+# ©  2008-2022 Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from . import account_edi_format
+from . import account_move
+from . import ir_actions_report
---
+++ l10n_ro_edi_ubl/models/ir_actions_report.py
@@ -0,0 +1,29 @@
+# ©  2008-2022 Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import models
+
+from lxml import etree
+import base64
+from xml.sax.saxutils import escape, quoteattr
+
+
+# class IrActionsReport(models.Model):
+#     _inherit = 'ir.actions.report'
+#
+#     def _postprocess_pdf_report(self, record, buffer):
+#         '''Add the pdf report in the e-fff XML as base64 string.
+#         '''
+#         result = super()._postprocess_pdf_report(record, buffer)
+#
+#         if record._name == 'account.move':
+#             edi_attachment = record.edi_document_ids.filtered(lambda d: d.edi_format_id.code == 'cirus_ro').attachment_id
+#             if edi_attachment:
+#                     edi_attachment.write({
+#                         'res_model': 'account.move',
+#                         'res_id': record.id,
+#                         'datas': base64.b64encode(new_xml),
+#                         'mimetype': 'application/xml',
+#                     })
+#
+#         return result
