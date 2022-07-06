9a1ec011088556e3bc63817fec0dc2f4e795f3b1
Dan Stoica <danila@terrabit.ro>
2022-07-06 11:42:03 +0300

    cancel text in cancelled credit note

--- l10n_ro_invoice_report/__manifest__.py
+++ l10n_ro_invoice_report/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Romania - Invoice Report Terrabit",
     "summary": "Localizare Terrabit",
-    "version": "15.0.3.0.7",
+    "version": "15.0.3.0.8",
     "author": "Dorin Hongu," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "license": "AGPL-3",
