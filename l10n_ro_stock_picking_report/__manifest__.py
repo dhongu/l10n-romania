e62563997ba9d2127f529a06c0c45322551b2eb4
Dorin Hongu <dhongu@gmail.com>
2022-07-22 09:17:17 +0300

    adaugare cod de bare

--- l10n_ro_stock_picking_report/__manifest__.py
+++ l10n_ro_stock_picking_report/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Romania - Picking Reports",
     "license": "AGPL-3",
-    "version": "15.0.1.0.7",
+    "version": "15.0.1.0.8",
     "author": "Dorin Hongu," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Generic Modules",
