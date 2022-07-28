ea5fe3701f151346725cbdafa6bd55520e330522
Dorin Hongu <dhongu@gmail.com>
2022-07-28 15:05:11 +0300

    adaugare camp notice

--- l10n_ro_stock_account_check/models/stock_picking.py
+++ l10n_ro_stock_account_check/models/stock_picking.py
@@ -2,12 +2,22 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
-from odoo import models
+from odoo import fields, models
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
+    notice = fields.Boolean(
+        "Is a notice",
+        states={"done": [("readonly", True)], "cancel": [("readonly", True)]},
+        default=False,
+        help="With this field the reception/delivery is set as a notice. "
+        "The generated account move will contain accounts 408/418.",
+    )
+
     def correction_valuation(self):
         for picking in self:
             picking.move_lines.correction_valuation()
+
+
