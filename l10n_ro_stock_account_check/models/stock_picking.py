26da591afa53d25488d94e1a2253204c84d5ef1b
Dorin Hongu <dhongu@gmail.com>
2022-07-28 15:11:49 +0300

    eliminare notice

--- l10n_ro_stock_account_check/models/stock_picking.py
+++ l10n_ro_stock_account_check/models/stock_picking.py
@@ -8,14 +8,6 @@ from odoo import fields, models
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    notice = fields.Boolean(
-        "Is a notice",
-        states={"done": [("readonly", True)], "cancel": [("readonly", True)]},
-        default=False,
-        help="With this field the reception/delivery is set as a notice. "
-        "The generated account move will contain accounts 408/418.",
-    )
-
     def correction_valuation(self):
         for picking in self:
             picking.move_lines.correction_valuation()
