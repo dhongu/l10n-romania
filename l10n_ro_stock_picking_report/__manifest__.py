e328610704e7c6ede5b848c328b9e728ee42a659
Dorin Hongu <dhongu@gmail.com>
2022-09-16 09:20:53 +0300

    fix

--- l10n_ro_stock_picking_report/__manifest__.py
+++ l10n_ro_stock_picking_report/__manifest__.py
@@ -4,8 +4,9 @@
 
 {
     "name": "Romania - Terrabit - Picking Reports",
+    "summary": "Rapoarte: NIR, aviz, bon consum",
     "license": "AGPL-3",
-    "version": "14.0.1.0.8",
+    "version": "14.0.1.0.9",
     "author": "Dorin Hongu," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Generic Modules",
@@ -23,7 +24,7 @@
         "views/l10n_ro_stock_picking_report.xml",
         "views/report_picking.xml",
         "views/stock_view.xml",
-        # "views/res_config_view.xml",
+        "views/res_config_view.xml",
         "views/stock_location_view.xml",
         # "report/stock_picking_cumulative_view.xml",
         # "security/ir.model.access.csv",
