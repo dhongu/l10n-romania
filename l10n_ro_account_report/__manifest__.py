ac8a4ed96bb4a548023ce3c64b75718ccb905330
Dorin Hongu <dhongu@gmail.com>
2022-11-15 06:09:09 +0200

    modificare iconite

--- l10n_ro_account_report/__manifest__.py
+++ l10n_ro_account_report/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Romania - Account Reports",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "author": "Terrabit," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Romania Adaptation",
