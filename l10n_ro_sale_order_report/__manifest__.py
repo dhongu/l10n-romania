8c65a46b7a73a0a9cee64e32c066f312aa8292a2
Dorin Hongu <dhongu@gmail.com>
2022-12-19 13:06:53 +0200

    utilizare ir.actions.report

---
+++ l10n_ro_sale_order_report/__manifest__.py
@@ -0,0 +1,17 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Terrabit Sale Order Report",
+    "summary": "Formular Factura Proformae",
+    "version": "15.0.1.0.0",
+    "author": "Dorin Hongu, Dan Stoica, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "category": "Sales",
+    "depends": ["sale", "l10n_ro_config", "deltatech_sale_payment"],
+    "license": "LGPL-3",
+    "data": ["views/sale_order.xml"],
+    "images": ["images/main_screenshot.png"],
+    "installable": True,
+}
