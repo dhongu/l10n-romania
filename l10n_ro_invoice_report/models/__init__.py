9fbf506fb7b48a2090d40111d3fd1690308f9314
Dorin Hongu <dhongu@gmail.com>
2023-01-20 09:36:41 +0200

    afisare telefon

--- l10n_ro_invoice_report/models/__init__.py
+++ l10n_ro_invoice_report/models/__init__.py
@@ -1,6 +1,7 @@
-# ©  2008-2020 Dorin Hongu <dhongu(@)gmail(.)com
+# ©  2008-2023 Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
 from . import res_partner
 from . import account_invoice
 from . import account_bank_statement
+from . import res_config
