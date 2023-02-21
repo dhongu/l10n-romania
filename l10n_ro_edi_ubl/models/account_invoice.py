512e197bce0b2bdc95aee586f75b9cdc0da0d035
Dorin Hongu <dhongu@gmail.com>
2023-02-21 17:29:07 +0200

    CIUS-RO:1.0.1

--- l10n_ro_edi_ubl/models/account_invoice.py
+++ l10n_ro_edi_ubl/models/account_invoice.py
@@ -49,7 +49,7 @@ class AccountInvoice(models.Model):
         ubl_version.text = version
 
         elem = etree.SubElement(parent_node, ns["cbc"] + "CustomizationID")
-        elem.text = "urn:cen.eu:en16931:2017#compliant#urn:efactura.mfinante.ro:CIUS-RO:1.0.0"
+        elem.text = "urn:cen.eu:en16931:2017#compliant#urn:efactura.mfinante.ro:CIUS-RO:1.0.1"
 
         doc_id = etree.SubElement(parent_node, ns["cbc"] + "ID")
         doc_id.text = self.number
