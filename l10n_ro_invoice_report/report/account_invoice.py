7e202cfc9b8b29290c15563aac4f6a68cef09725
Dorin Hongu <dhongu@gmail.com>
2023-02-21 12:27:51 +0200

    tiparire factura

--- l10n_ro_invoice_report/report/account_invoice.py
+++ l10n_ro_invoice_report/report/account_invoice.py
@@ -57,73 +57,9 @@ class ReportInvoiceWithoutPayment(models.AbstractModel):
         return show_discount
 
 
-# class ReportInvoiceWithPaymentsPrint(models.AbstractModel):
-#     _name = "report.account.report_invoice_with_payments"
-#     _description = "ReportInvoiceWithPaymentsPrint"
-#     _template = "account.report_invoice_with_payments"
-#
-#     @api.model
-#     def _get_report_values(self, docids, data=None):
-#         report = self.env["ir.actions.report"]._get_report_from_name(self._template)
-#         return {
-#             "doc_ids": docids,
-#             "doc_model": report.model,
-#             "data": data,
-#             "time": time,
-#             "docs": self.env[report.model].browse(docids),
-#             # "convert": self._convert,
-#             "with_discount": self._with_discount,
-#             "amount_to_text": self._amount_to_text,
-#             "get_pickings": self._get_pickings,
-#             "get_discount": self._get_discount(),
-#         }
-#
-#     def _amount_to_text(self, amount, currency):
-#         return currency.amount_to_text(amount)
-#
-#     # def _convert(self, amount):
-#     #     # todo: de folosit libraria num2words dupa ce o sa aiba si limba romana
-#     #     amt_ro = amount_to_text_ro.amount_to_text_ro(amount)
-#     #     return amt_ro
-#
-#     def _with_discount(self, invoice):
-#         res = False
-#         for line in invoice.invoice_line_ids:
-#             if line.discount != 0.0:
-#                 res = True
-#         return res
-#
-#     def _get_pickings(self, invoice):
-#
-#         if (
-#             not self.env["ir.module.module"]
-#             .sudo()
-#             .search([("name", "=", "stock"), ("state", "=", "installed")])
-#         ):
-#             return False
-#
-#         pickings = self.env["stock.picking"]
-#         for line in invoice.invoice_line_ids:
-#             for sale_line in line.sale_line_ids:
-#                 for move in sale_line.move_ids:
-#                     if move.picking_id.state == "done":
-#                         pickings |= move.picking_id
-#             if line.purchase_line_id:
-#                 for move in line.purchase_line_id.move_ids:
-#                     if move.picking_id.state == "done":
-#                         pickings |= move.picking_id
-#         return pickings
-#
-#     def _get_discount(self):
-#         config_parameter = (
-#             self.env["ir.config_parameter"]
-#             .sudo()
-#             .search([("key", "=", "l10n_ro_config.show_discount")])
-#         )
-#         return config_parameter.value
+class ReportInvoicePrintInCompanyLanguage(models.AbstractModel):
+    _name = "report.l10n_ro_invoice_report.report_invoice_company_language"
+    _description = "Report Invoice in Company Language"
+    _inherit = "report.account.report_invoice"
+    _template = "l10n_ro_invoice_report.report_invoice_company_language"
 
-
-# class ReportInvoicePrint(ReportInvoiceWithPaymentsPrint):
-#     _name = "report.account.report_invoice"
-#     _description = "ReportInvoicePrint"
-#     _template = "account.report_invoice"
