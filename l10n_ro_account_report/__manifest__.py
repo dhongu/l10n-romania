bc77faffe02ce53ad632bef13b2624492239e560
Dorin Hongu <dhongu@gmail.com>
2023-06-06 08:52:25 +0300

    eliminare date_range

--- l10n_ro_account_report/__manifest__.py
+++ l10n_ro_account_report/__manifest__.py
@@ -1,11 +1,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Romania - Account Reports",
-    "version": "15.0.1.0.1",
+    "version": "15.0.1.0.2",
     "author": "Terrabit," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Romania Adaptation",
-    "depends": ["account", "date_range"],
+    "depends": ["account"],
     "data": [
         "views/account_report.xml",
         "views/report_bank_statement.xml",
