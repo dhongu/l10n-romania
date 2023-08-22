941d5c1c4c57a70c2a83fcc99e14c597081d4862
danila12 <danila@terrabit.ro>
2023-08-22 11:43:06 +0300

    inherited _prepare_move_lines

--- l10n_ro_reconcile_order/wizard/account_reconciliation.py
+++ l10n_ro_reconcile_order/wizard/account_reconciliation.py
@@ -2,35 +2,14 @@
 
 
 from odoo import api, models
-from odoo.tools.misc import format_date, parse_date
 
 
 class AccountReconciliation(models.AbstractModel):
     _inherit = "account.reconciliation.widget"
 
     @api.model
-    def get_move_lines_for_manual_reconciliation(
-        self,
-        account_id,
-        partner_id=False,
-        excluded_ids=None,
-        search_str=False,
-        offset=0,
-        limit=None,
-        target_currency_id=False,
-    ):
-        lines = super().get_move_lines_for_manual_reconciliation(
-            account_id=account_id,
-            partner_id=partner_id,
-            excluded_ids=excluded_ids,
-            search_str=search_str,
-            offset=offset,
-            limit=limit,
-            target_currency_id=target_currency_id,
+    def _prepare_move_lines(self, move_lines, target_currency=False, target_date=False, recs_count=0):
+        move_lines = move_lines.sorted(key=lambda m: m.date_maturity or m.date)
+        return super(AccountReconciliation, self)._prepare_move_lines(
+            move_lines, target_currency, target_date, recs_count
         )
-        for line in lines:
-            line["date_maturity"] = parse_date(self.env, line["date_maturity"])
-        ordered_lines = sorted(lines, key=lambda d: d["date_maturity"])
-        for line in ordered_lines:
-            line["date_maturity"] = format_date(self.env, line["date_maturity"])
-        return ordered_lines
