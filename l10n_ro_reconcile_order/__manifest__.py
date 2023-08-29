cc5da5ae75729ae4229bc7ced6a790079db4ff2e
danila12 <danila@terrabit.ro>
2023-08-29 12:29:15 +0300

    modif order

--- l10n_ro_reconcile_order/__manifest__.py
+++ l10n_ro_reconcile_order/__manifest__.py
@@ -4,8 +4,8 @@
 
 {
     "name": "Terrabit Reconciliation Items Order",
-    "summary": "Order lines by maturity date ascending in reconciliation widget",
-    "version": "15.0.1.0.0",
+    "summary": "Order lines by date ascending in reconciliation widget",
+    "version": "15.0.1.0.1",
     "author": "Dan Stoica, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Account",
