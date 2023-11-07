4920999993af89b39ce9d3b6133588b71b5770ad
danila12 <danila@terrabit.ro>
2023-11-07 15:24:01 +0200

    removed check_valuation_layer()

--- l10n_ro_stock_account_check/__manifest__.py
+++ l10n_ro_stock_account_check/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Romania - Stock Accounting Check",
     "license": "AGPL-3",
-    "version": "15.0.1.0.6",
+    "version": "15.0.1.0.7",
     "author": "Terrabit,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Warehouse",
