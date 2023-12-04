ff7ff28fb0fdf150116f1f26838bc85733be9d25
danila12 <danila@terrabit.ro>
2023-12-04 13:49:32 +0200

    added parallel valuation module

--- l10n_ro_invoice_report/models/account_invoice.py
+++ l10n_ro_invoice_report/models/account_invoice.py
@@ -81,7 +81,7 @@ class AccountInvoice(models.Model):
         return values
 
     def _get_reconciled_info_JSON_values(self):
-        reconciled_vals = super(AccountInvoice, self)._get_reconciled_info_JSON_values()
+        reconciled_vals = super()._get_reconciled_info_JSON_values()
         for reconciled_val in reconciled_vals:
             payment = self.env["account.payment"].browse(reconciled_val["account_payment_id"])
             reconciled_val["journal_type"] = payment.journal_id.type
