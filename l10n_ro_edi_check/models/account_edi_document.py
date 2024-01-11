00620f1b73023bff0418702264244d226c97c007
danila12 <danila@terrabit.ro>
2024-01-11 10:45:52 +0200

    added stock_inventory_report module

--- l10n_ro_edi_check/models/account_edi_document.py
+++ l10n_ro_edi_check/models/account_edi_document.py
@@ -2,25 +2,22 @@
 # See README.rst file on addons root folder for license details
 
 
-
-
-from odoo import models, api, fields
-
 import logging
+
+from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
 
-
 class AccountEdiDocument(models.Model):
-    _inherit = 'account.edi.document'
+    _inherit = "account.edi.document"
 
     @api.model
     def _cron_process_documents_web_services(self, job_count=None):
         domain = [
-            ('state', 'in', ('to_send', 'to_cancel')),
-            ('move_id.state', '=', 'posted'),
-            ("move_id.date", ">=", "2024-01-01")
+            ("state", "in", ("to_send", "to_cancel")),
+            ("move_id.state", "=", "posted"),
+            ("move_id.date", ">=", "2024-01-01"),
         ]
         edi_documents = self.search(domain)
 
@@ -28,16 +25,13 @@ class AccountEdiDocument(models.Model):
             edi_document.with_delay()._process_documents_web_services()
 
         domain = [
-            ('state', 'in', ('to_send', 'to_cancel')),
-            ('move_id.state', '=', 'posted'),
+            ("state", "in", ("to_send", "to_cancel")),
+            ("move_id.state", "=", "posted"),
             ("blocking_level", "=", "error"),
-            ("write_date", "<", fields.Date.today())
+            ("write_date", "<", fields.Date.today()),
         ]
         edi_documents = self.search(domain)
         if edi_documents:
             edi_documents.write({"blocking_level": False})
 
-        self.env.ref('queue_job_cron_jobrunner.queue_job_cron')._trigger()
-
-
-
+        self.env.ref("queue_job_cron_jobrunner.queue_job_cron")._trigger()
