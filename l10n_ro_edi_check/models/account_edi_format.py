0f610c0f2b60d3a449777ed0b5fe743b4afdf1f6
Dorin Hongu <dhongu@gmail.com>
2024-01-12 04:52:37 +0200

    fix namespaces

---
+++ l10n_ro_edi_check/models/account_edi_format.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2022 Dorin Hongu <dhongu(@)gmail(.)com
+# Copyright (C) 2022 NextERP Romania
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import logging
+
+
+
+from odoo import models
+
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountEdiXmlCIUSRO(models.Model):
+    _inherit = "account.edi.format"
+
+    def _find_value(self, xpath, xml_element, namespaces=None):
+        if namespaces is None:
+            namespaces = {}
+
+        namespaces.update({
+            "qdt": "urn:oasis:names:specification:ubl:schema:xsd:QualifiedDataTypes-2",
+            "ccts": "urn:un:unece:uncefact:documentation:2",
+            "udt": "urn:oasis:names:specification:ubl:schema:xsd:UnqualifiedDataTypes-2",
+            "cac": "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2",  # noqa: B950
+            "cbc": "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2",
+            "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+        })
+
+        res = super(AccountEdiXmlCIUSRO, self)._find_value(xpath, xml_element, namespaces=namespaces)
+
+        return res
