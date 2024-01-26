f6339ff5a8f3930cd3164055bab4b68f32746326
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-01-26 14:06:52 +0200

    Removed key from default

--- l10n_ro_partner_create_by_vat_openapi/models/res_partner.py
+++ l10n_ro_partner_create_by_vat_openapi/models/res_partner.py
@@ -53,7 +53,7 @@ class ResPartner(models.Model):
         openapi_key = (
             self.env["ir.config_parameter"]
             .sudo()
-            .get_param(key="openapi_key", default="2bsLug_vmT4qBVKNnAGpmvNMQPJJWd5x5yMi9swHEb6hx322zg")
+            .get_param(key="openapi_key", default="False")
         )
 
         if not openapi_key:
