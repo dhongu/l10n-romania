3650a3d9084f44e231a799a38090331ea52c5d81
Dorin Hongu <dhongu@gmail.com>
2024-02-01 08:02:54 +0200

    intrastat

--- l10n_ro_intrastat/models/account_intrastat_code.py
+++ l10n_ro_intrastat/models/account_intrastat_code.py
@@ -1,7 +1,7 @@
 # ©  2008-2020 Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class AccountIntrastatCode(models.Model):
@@ -20,34 +20,68 @@ class AccountIntrastatCode(models.Model):
     name = fields.Char(string="Name")
     nckey = fields.Char(string="NC Key")
     code = fields.Char(string="NC Code", required=True)
-
+    country_id = fields.Many2one(
+        "res.country",
+        string="Country",
+        help="Restrict the applicability of code to a country.",
+        domain="[('intrastat', '=', True)]",
+    )
     description = fields.Char(string="Description")
     suppl_unit_code = fields.Char("SupplUnitCode")
+    type = fields.Selection(
+        string="Type",
+        required=True,
+        selection=[
+            ("commodity", "Commodity"),
+            ("transport", "Transport"),
+            ("transaction", "Transaction"),
+            ("region", "Region"),
+        ],
+        default="commodity",
+        help="""Type of intrastat code used to filter codes by usage.
+            * commodity: Code to be set on invoice lines for European Union statistical purposes.
+            * transport: The active vehicle that moves the goods across the border.
+            * transaction: A movement of goods.
+            * region: A sub-part of the country.
+        """,
+    )
+
+    expiry_date = fields.Date(
+        string="Expiry Date",
+        help="Date at which a code must not be used anymore.",
+    )
+    start_date = fields.Date(
+        string="Usage start date",
+        help="Date from which a code may be used.",
+    )
 
     def name_get(self):
         result = []
         for r in self:
-            text = r.name or r.description
+            if r.name == r.code:
+                text = r.description
+            else:
+                text = r.name
             result.append((r.id, text and "{} {}".format(r.code, text) or r.code))
         return result
 
-    @api.model
-    def _name_search(self, name="", args=None, operator="ilike", limit=100):
-        if args is None:
-            args = []
-        domain = args + [
-            "|",
-            "|",
-            ("code", operator, name),
-            ("name", operator, name),
-            ("description", operator, name),
-        ]
-        return super().search(domain, limit=limit).name_get()
-
-    _sql_constraints = [
-        (
-            "intrastat_region_nckey_unique",
-            "UNIQUE (nckey)",
-            "The NC key must be unique.",
-        ),
-    ]
+    # @api.model
+    # def _name_search(self, name="", args=None, operator="ilike", limit=100):
+    #     if args is None:
+    #         args = []
+    #     domain = args + [
+    #         "|",
+    #         "|",
+    #         ("code", operator, name),
+    #         ("name", operator, name),
+    #         ("description", operator, name),
+    #     ]
+    #     return super(AccountIntrastatCode, self).search(domain, limit=limit).name_get()
+    #
+    # _sql_constraints = [
+    #     (
+    #         "intrastat_region_nckey_unique",
+    #         "UNIQUE (nckey)",
+    #         "The NC key must be unique.",
+    #     ),
+    # ]
--- l10n_ro_intrastat_xml/tests/test_intrastat.py
+++ l10n_ro_intrastat_xml/tests/test_intrastat.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 
+from odoo.exceptions import UserError
 from odoo.tests import Form
 from odoo.tests.common import TransactionCase
 
@@ -11,7 +12,11 @@ class TestIntrastat(TransactionCase):
         super().setUp()
         self.env.user.company_id.write({"vat": "RO20603502"})
         self.intrastat = self.env["account.intrastat.code"].create(
-            {"name": "test", "code": "84221100", "suppl_unit_code": "p/st"}
+            {
+                "name": "test",
+                "code": "84221100",
+                # "suppl_unit_code": "p/st",
+            }
         )
 
         self.product_1 = self.env["product.product"].create(
@@ -36,7 +41,7 @@ class TestIntrastat(TransactionCase):
 
         invoice = Form(self.env["account.move"].with_context(default_type="in_invoice"))
         invoice.partner_id = partner_de
-        invoice.intrastat_transaction_id = self.env.ref("l10n_ro_intrastat.intrastat_transaction_1_1")
+        # invoice.intrastat_transaction_id = self.env.ref("l10n_ro_intrastat.intrastat_transaction_1_1")
         invoice.transport_mode_id = self.env.ref("l10n_ro_intrastat.intrastat_trmode_3")
         invoice.invoice_incoterm_id = self.env.ref("account.incoterm_EXW")
 
@@ -50,6 +55,11 @@ class TestIntrastat(TransactionCase):
 
         # a facut onchange ?
         self.assertEqual(invoice.intrastat_country_id, country)
+
+        wizard = Form(self.env["l10n.ro.intrastat.xml.declaration"])
+        wizard.contact_id = self.env.user.partner_id
+        wizard = wizard.save()
+        wizard.create_xml()
 
     def test_invoice_sale(self):
         country = self.env.ref("base.de")
@@ -67,9 +77,52 @@ class TestIntrastat(TransactionCase):
         so.action_confirm()
         invoice = so._create_invoices(final=True)
         invoice = Form(invoice)
-        invoice.intrastat_transaction_id = self.env.ref("l10n_ro_intrastat.intrastat_transaction_1_1")
-        invoice.transport_mode_id = self.env.ref("l10n_ro_intrastat.intrastat_trmode_3")
+        # invoice.intrastat_transaction_id = self.env.ref("l10n_ro_intrastat.intrastat_transaction_1_1")
+        # invoice.transport_mode_id = self.env.ref("l10n_ro_intrastat.intrastat_trmode_3")
         invoice.invoice_incoterm_id = self.env.ref("account.incoterm_EXW")
 
         invoice = invoice.save()
         invoice.post()
+
+        wizard = Form(self.env["l10n.ro.intrastat.xml.declaration"])
+        wizard.contact_id = self.env.user.partner_id
+        wizard.type = "dispatches"
+        wizard = wizard.save()
+        wizard.create_xml()
+
+    def test_error(self):
+        country = self.env.ref("base.de")
+        partner_de = self.env["res.partner"].create({"name": "TEST Vendor", "country_id": country.id})
+
+        invoice = Form(self.env["account.move"].with_context(default_type="in_invoice"))
+        invoice.partner_id = partner_de
+
+        with invoice.invoice_line_ids.new() as line_form:
+            line_form.product_id = self.product_2
+            line_form.price_unit = 10
+            line_form.quantity = 20
+
+        invoice = invoice.save()
+        invoice.post()
+
+        wizard = Form(self.env["l10n.ro.intrastat.xml.declaration"])
+        wizard.contact_id = self.env.user.partner_id
+        wizard = wizard.save()
+
+        with self.assertRaises(UserError):
+            wizard.create_xml()
+
+        intrastat_transaction = self.env.ref("l10n_ro_intrastat.intrastat_transaction_1_1")
+        invoice.write({"intrastat_transaction_id": intrastat_transaction.id})
+        with self.assertRaises(UserError):
+            wizard.create_xml()
+
+        transport_mode = self.env.ref("l10n_ro_intrastat.intrastat_trmode_3")
+        invoice.write({"transport_mode_id": transport_mode.id})
+        with self.assertRaises(UserError):
+            wizard.create_xml()
+
+        invoice_incoterm = self.env.ref("account.incoterm_EXW")
+        invoice.write({"invoice_incoterm_id": invoice_incoterm.id})
+        with self.assertRaises(UserError):
+            wizard.create_xml()
