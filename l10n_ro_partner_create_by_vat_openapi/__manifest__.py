0ea8119d93cab9ac386f0f6e13bb05220c0f471b
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-02-26 14:13:53 +0200

    [NEW] partner_create_by_vat_openapi

---
+++ l10n_ro_partner_create_by_vat_openapi/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright  2015 Forest and Biomass Romania
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Romania - Partner Create by VAT",
+    "summary": "Romania - Partner Create by VAT",
+    "version": "15.0.1.0.0",
+    "author": "Forest and Biomass Romania, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "category": "Localization",
+    "depends": [
+        "base_vat",
+        "l10n_ro_config",
+    ],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+    "application": False,
+    "auto_install": False,
+}
