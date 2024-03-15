bbe0d0439ae4a47c28d5c75ffed89357bfa51dcb
Dorin Hongu <dhongu@gmail.com>
2024-03-15 10:44:14 +0200

    adaugare header

---
+++ l10n_ro_partner/__manifest__.py
@@ -0,0 +1,14 @@
+# ©  2024 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Romania - Partner",
+    "license": "AGPL-3",
+    "version": "16.0.0.0.0",
+    "author": "Dorin Hongu," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "category": "Localization",
+    "depends": [],
+    "data": ["views/res_partner_view.xml"],
+}
