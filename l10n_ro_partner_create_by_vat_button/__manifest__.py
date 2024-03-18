d515fbd0d85fbec69e704338647d7f6f88caaf10
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-03-18 17:48:56 +0200

    [15.0][UPD] create_by_button

--- l10n_ro_partner_create_by_vat_button/__manifest__.py
+++ l10n_ro_partner_create_by_vat_button/__manifest__.py
@@ -5,10 +5,13 @@
 {
     "name": "Romania - Partner Create by VAT Button",
     "license": "AGPL-3",
-    "version": "15.0.1.0.3",
+    "version": "15.0.1.0.4",
     "author": "Dorin Hongu," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Localization",
-    "depends": ["l10n_ro_partner_create_by_vat"],
-    "data": ["views/res_partner_view.xml"],
+    "depends": ["l10n_ro_partner_create_by_vat",
+                "l10n_ro_partner_create_by_vat_openapi"],
+    "data": ["security/ir.model.access.csv",
+             "views/res_partner_view.xml",
+             "wizard/get_data_form_view.xml"],
 }
