475857790b22e8de3285026e1775243eff28417f
Dorin Hongu <dhongu@gmail.com>
2024-03-19 18:40:40 +0200

    fix

--- l10n_ro_partner_create_by_vat_button/__manifest__.py
+++ l10n_ro_partner_create_by_vat_button/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Romania - Partner Create by VAT Button",
     "license": "AGPL-3",
-    "version": "15.0.1.0.4",
+    "version": "15.0.1.0.5",
     "author": "Dorin Hongu," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Localization",
