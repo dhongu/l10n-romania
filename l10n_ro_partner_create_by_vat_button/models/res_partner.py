143acb6647ec61a871ab9bad30732ba26e6a58b1
Dorin Hongu <dhongu@gmail.com>
2024-03-25 10:57:35 +0200

    modificari din 14.0

--- l10n_ro_partner_create_by_vat_button/models/res_partner.py
+++ l10n_ro_partner_create_by_vat_button/models/res_partner.py
@@ -5,8 +5,10 @@
 
 import logging
 
+from zeep import Client
+
 from odoo import _, api, models
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
@@ -48,10 +50,24 @@ class ResPartner(models.Model):
             return False
         if self.name and not self.vat:
             self.vat = self.name
-        res = self.with_context(skip_ro_vat_change=False).ro_vat_change()
-        if "warning" in res:
-            raise ValidationError(_("ANAF Webservice not working.\nPlease try again until it works."))
+        self.with_context(skip_ro_vat_change=False).ro_vat_change()
+
+        return True
         # self.onchange_vat_subjected()  # fortare compltare ro
+
+    def get_partner_name_from_vies(self):
+        # Create a client for the VIES SOAP service
+        client = Client("http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl")
+
+        # Make a request to the VIES service to check the VAT number
+        response = client.service.checkVat(countryCode=self.name[:2], vatNumber=self.name[2:])
+        if response.valid:
+            self.vat = self.name
+            self.country_id = self.env["res.country"].search([("code", "ilike", self.name[:2].lower())])[0].id
+            self.name = response.name
+            self.street = response.address
+        else:
+            raise UserError(_("Invalid VAT"))
 
     @api.onchange("vat", "country_id")
     def ro_vat_change(self):
---
+++ l10n_ro_partner_create_by_vat_button/wizard/get_partner_data.py
@@ -0,0 +1,28 @@
+from odoo import _, fields, models
+from odoo.exceptions import UserError
+
+
+class GetPartnerData(models.TransientModel):
+    _name = "get.partner.data"
+    _description = "Get partner data from"
+
+    service = fields.Selection(
+        [("anaf", "ANAF"), ("openapi", "OpenAPI"), ("vies", "VIES for non-Romanian partners")],
+        default="anaf",
+        string="Service",
+    )
+
+    def do_get_data(self):
+        active_ids = self.env.context.get("active_ids", [])
+        active_model = self.env.context.get("active_model", "res.partner")
+        partner = self.env[active_model].browse(active_ids)
+        if self.service == "anaf":
+            partner.button_get_partner_data()
+        if self.service == "openapi":
+            openapi_key = self.env["ir.config_parameter"].sudo().get_param(key="openapi_key", default=False)
+            if not openapi_key:
+                raise UserError(_("API Key is missing - please contact support service!"))
+            partner.button_get_partner_data_openapi()
+        if self.service == "vies":
+            partner.get_partner_name_from_vies()
+        return
