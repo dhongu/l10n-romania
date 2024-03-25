e18463a337cd80660fc8f1b09cfe98444af310a6
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-03-25 08:55:50 +0200

    [15.0][UPD]l10n_ro_partner_create_by_vat_button: Vies option

--- l10n_ro_partner_create_by_vat_button/models/res_partner.py
+++ l10n_ro_partner_create_by_vat_button/models/res_partner.py
@@ -4,8 +4,9 @@
 
 
 import logging
-
-from odoo import api, models
+from zeep import Client
+from odoo import _, api, models
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
@@ -51,6 +52,21 @@ class ResPartner(models.Model):
 
         return True
         # self.onchange_vat_subjected()  # fortare compltare ro
+
+    def get_partner_name_from_vies(self):
+
+        # Create a client for the VIES SOAP service
+        client = Client('http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl')
+
+        # Make a request to the VIES service to check the VAT number
+        response = client.service.checkVat(countryCode=self.name[:2], vatNumber=self.name[2:])
+        if response.valid:
+            self.vat = self.name
+            self.country_id = self.env["res.country"].search([("code", "ilike", self.name[:2].lower())])[0].id
+            self.name = response.name
+            self.street = response.address
+        else:
+            raise UserError(_("Invalid VAT"))
 
     @api.onchange("vat", "country_id")
     def ro_vat_change(self):
