35f43db57df9cae69888f5eb72587130ca34fc1b
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-03-28 08:52:14 +0200

    pre-commit

--- l10n_ro_partner_create_by_vat_openapi/wizard/get_partner_data.py
+++ l10n_ro_partner_create_by_vat_openapi/wizard/get_partner_data.py
@@ -8,7 +8,7 @@ class GetPartnerData(models.TransientModel):
     service = fields.Selection(selection_add=[("openapi", "OpenAPI")])
 
     def do_get_data(self):
-        partner=super().do_get_data()
+        partner = super().do_get_data()
         if self.service == "openapi":
             openapi_key = self.env["ir.config_parameter"].sudo().get_param(key="openapi_key", default=False)
             if not openapi_key:
