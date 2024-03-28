f2204ceca871eedd6170c0247adf1ec3ac03694d
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-03-28 09:55:17 +0200

    [15.0][UPD] openapi and button

--- l10n_ro_partner_create_by_vat_button/__manifest__.py
+++ l10n_ro_partner_create_by_vat_button/__manifest__.py
@@ -5,10 +5,17 @@
 {
     "name": "Romania - Partner Create by VAT Button",
     "license": "AGPL-3",
-    "version": "15.0.1.0.5",
+    "version": "16.0.1.0.6",
     "author": "Dorin Hongu," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Localization",
-    "depends": ["l10n_ro_partner_create_by_vat", "l10n_ro_partner_create_by_vat_openapi"],
-    "data": ["security/ir.model.access.csv", "views/res_partner_view.xml", "wizard/get_data_form_view.xml"],
+    "depends": [
+        "l10n_ro_partner_create_by_vat",
+        # "l10n_ro_partner_create_by_vat_openapi"
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/get_data_form_view.xml",
+        "views/res_partner_view.xml",
+    ],
 }
--- l10n_ro_partner_create_by_vat_button/wizard/get_partner_data.py
+++ l10n_ro_partner_create_by_vat_button/wizard/get_partner_data.py
@@ -1,5 +1,4 @@
-from odoo import _, fields, models
-from odoo.exceptions import UserError
+from odoo import fields, models,api
 
 
 class GetPartnerData(models.TransientModel):
@@ -7,22 +6,25 @@ class GetPartnerData(models.TransientModel):
     _description = "Get partner data from"
 
     service = fields.Selection(
-        [("anaf", "ANAF"), ("openapi", "OpenAPI"), ("vies", "VIES for non-Romanian partners")],
+        [("anaf", "ANAF"), ("vies", "VIES for non-Romanian partners")],
         default="anaf",
         string="Service",
     )
 
-    def do_get_data(self):
+    def default_get(self, fields):
+        res = super(GetPartnerData, self).default_get(fields)
         active_ids = self.env.context.get("active_ids", [])
         active_model = self.env.context.get("active_model", "res.partner")
         partner = self.env[active_model].browse(active_ids)
+        if partner:
+            res['partner_id'] = partner.id
+        return res
+
+    partner_id = fields.Many2one('res.partner', string='Partner')
+
+    def do_get_data(self):
         if self.service == "anaf":
-            partner.button_get_partner_data()
-        if self.service == "openapi":
-            openapi_key = self.env["ir.config_parameter"].sudo().get_param(key="openapi_key", default=False)
-            if not openapi_key:
-                raise UserError(_("API Key is missing - please contact support service!"))
-            partner.button_get_partner_data_openapi()
+            self.partner_id.get_partner_data()
         if self.service == "vies":
-            partner.get_partner_name_from_vies()
+            self.partner_id.get_partner_name_from_vies()
         return
