b1755a86f22b2ab386897f910061e7ab26ff92f0
Dorin Hongu <dhongu@gmail.com>
2024-04-07 09:47:06 +0300

    update

--- l10n_ro_edi_check/models/account_move.py
+++ l10n_ro_edi_check/models/account_move.py
@@ -8,26 +8,31 @@ from odoo.exceptions import UserError
 class AccountMove(models.Model):
     _inherit = "account.move"
 
+    def _l10n_ro_prepare_invoice_for_download(self):
+        res = super()._l10n_ro_prepare_invoice_for_download()
+        return res.with_delay()
+
     def action_post(self):
         res = super().action_post()
         errors = []
         for move in self:
             if move.move_type in ["out_invoice", "out_refund"] and move.commercial_partner_id.is_company:
                 country_ro = self.env.ref("base.ro")
-                partner = move.commercial_partner_id
-                if not partner.country_id:
-                    errors += [_("Partenerul %s nu are completata tara") % partner.name]
-                elif partner.country_id == country_ro:
-                    if not partner.street:
-                        errors += [_("Partenerul %s nu are completata strada") % partner.name]
+                parteners = list(set([move.partner_id, move.commercial_partner_id, move.partner_shipping_id]))
+                for partner in parteners:
+                    if not partner.country_id:
+                        errors += [_("Partenerul %s nu are completata tara") % partner.name]
+                    elif partner.country_id == country_ro:
+                        if not partner.street:
+                            errors += [_("Partenerul %s nu are completata strada") % partner.name]
 
-                    if not partner.city:
-                        errors += [_("Partenerul %s nu are completata localitatea") % partner.name]
+                        if not partner.city:
+                            errors += [_("Partenerul %s nu are completata localitatea") % partner.name]
 
-                    state_bucuresti = self.env.ref("base.RO_B")
-                    if partner.state_id == state_bucuresti and partner.city:
-                        if "sector" not in partner.city.lower():
-                            errors += [_("localitatea pertenerului %s trebuie sa fie de forma SectorX ") % partner.name]
+                        state_bucuresti = self.env.ref("base.RO_B")
+                        if partner.state_id == state_bucuresti and partner.city:
+                            if "sector" not in partner.city.lower():
+                                errors += [_("localitatea pertenerului %s trebuie sa fie de forma SectorX ") % partner.name]
                 if errors:
                     errors_text = "\n".join(errors)
                     raise UserError(errors_text)
