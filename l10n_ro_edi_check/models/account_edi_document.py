a02905e05113f7853f1787186a12853881f8ecbb
Dorin Hongu <dhongu@gmail.com>
2024-06-06 11:14:02 +0300

    fix

--- l10n_ro_edi_check/models/account_edi_document.py
+++ l10n_ro_edi_check/models/account_edi_document.py
@@ -9,33 +9,23 @@ from odoo import api, models
 _logger = logging.getLogger(__name__)
 
 
-class AccountEdiDocument(models.Model):
-    _inherit = "account.edi.document"
-
-    @api.model
-    def _cron_process_documents_web_services(self, job_count=None):
-        domain = [
-            ("state", "in", ("to_send", "to_cancel")),
-            ("move_id.state", "=", "posted"),
-            ("move_id.date", ">=", "2024-01-01"),
-        ]
-        edi_documents = self.search(domain)
-
-        for edi_document in edi_documents:
-            key = "ro_efactura_{}".format(edi_document.move_id.id)
-            domain = [("identity_key", "=", key), ("state", "=", "failed")]
-            existing = self.env["queue.job"].sudo().search(domain, limit=1)
-            if not existing:
-                edi_document.with_delay(identity_key=key)._process_documents_web_services()
-
-        # domain = [
-        #     ("state", "in", ("to_send", "to_cancel")),
-        #     ("move_id.state", "=", "posted"),
-        #     ("blocking_level", "=", "error"),
-        #     ("write_date", "<", fields.Date.today()),
-        # ]
-        # edi_documents = self.search(domain)
-        # if edi_documents:
-        #     edi_documents.write({"blocking_level": False})
-
-        self.env.ref("queue_job_cron_jobrunner.queue_job_cron")._trigger()
+# class AccountEdiDocument(models.Model):
+#     _inherit = "account.edi.document"
+#
+#     @api.model
+#     def _cron_process_documents_web_services(self, job_count=None):
+#         domain = [
+#             ("state", "in", ("to_send", "to_cancel")),
+#             ("move_id.state", "=", "posted"),
+#             ("move_id.date", ">=", "2024-01-01"),
+#         ]
+#         edi_documents = self.search(domain)
+#
+#         for edi_document in edi_documents:
+#             key = "ro_efactura_{}".format(edi_document.move_id.id)
+#             domain = [("identity_key", "=", key), ("state", "=", "failed")]
+#             existing = self.env["queue.job"].sudo().search(domain, limit=1)
+#             if not existing:
+#                 edi_document.with_delay(identity_key=key)._process_documents_web_services()
+#
+#         self.env.ref("queue_job_cron_jobrunner.queue_job_cron")._trigger()
--- l10n_ro_edi_check/models/account_edi_format.py
+++ l10n_ro_edi_check/models/account_edi_format.py
@@ -9,24 +9,24 @@ from odoo import models
 _logger = logging.getLogger(__name__)
 
 
-class AccountEdiXmlCIUSRO(models.Model):
-    _inherit = "account.edi.xml.cius_ro"
-
-    def _get_delivery_vals_list(self, invoice):
-        res = super()._get_delivery_vals_list(invoice)
-
-        shipping_address = False
-        if "partner_shipping_id" in invoice._fields and invoice.partner_shipping_id:
-            shipping_address = invoice.partner_shipping_id
-            if shipping_address == invoice.partner_id:
-                shipping_address = False
-        if shipping_address:
-            res = [
-                {
-                    "actual_delivery_date": invoice.invoice_date,
-                    "delivery_location_vals": {
-                        "delivery_address_vals": self._get_partner_address_vals(shipping_address),
-                    },
-                }
-            ]
-        return res
+# class AccountEdiXmlCIUSRO(models.Model):
+#     _inherit = "account.edi.xml.cius_ro"
+#
+#     def _get_delivery_vals_list(self, invoice):
+#         res = super()._get_delivery_vals_list(invoice)
+#
+#         shipping_address = False
+#         if "partner_shipping_id" in invoice._fields and invoice.partner_shipping_id:
+#             shipping_address = invoice.partner_shipping_id
+#             if shipping_address == invoice.partner_id:
+#                 shipping_address = False
+#         if shipping_address:
+#             res = [
+#                 {
+#                     "actual_delivery_date": invoice.invoice_date,
+#                     "delivery_location_vals": {
+#                         "delivery_address_vals": self._get_partner_address_vals(shipping_address),
+#                     },
+#                 }
+#             ]
+#         return res
