2eb05d81a8f23a021dcb31312c8a9875ef49c485
Dorin Hongu <dhongu@gmail.com>
2024-06-07 04:55:29 +0300

    efactura

---
+++ l10n_ro_account_edi_ubl/__manifest__.py
@@ -0,0 +1,16 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'author': 'Odoo',
+    'name': 'Romania - Send E-Factura',
+    'version': '1.0',
+    'category': 'Accounting/Localizations/EDI',
+    'summary': "Bridge module for sending Romanian E-Factura to the SPV",
+    'countries': ['ro'],
+    'depends': ['l10n_ro_efactura'],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
