5f52eb4dd8e53873909358c76b53c93ff7576603
danila12 <danila@terrabit.ro>
2024-07-17 14:22:47 +0300

    fix picking report error some code cleaning

--- l10n_ro_account_edi_ubl/__manifest__.py
+++ l10n_ro_account_edi_ubl/__manifest__.py
@@ -1,16 +1,16 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'author': 'Odoo',
-    'name': 'Romania - Send E-Factura',
-    'version': '1.0',
-    'category': 'Accounting/Localizations/EDI',
-    'summary': "Bridge module for sending Romanian E-Factura to the SPV",
-    'countries': ['ro'],
-    'depends': ['l10n_ro_efactura'],
-    'data': [
-    ],
-    'installable': True,
-    'auto_install': True,
-    'license': 'LGPL-3',
+    "author": "Odoo",
+    "name": "Romania - Send E-Factura - legacy",
+    "version": "1.0",
+    "category": "Accounting/Localizations/EDI",
+    "summary": "Bridge module for sending Romanian E-Factura to the SPV",
+    "countries": ["ro"],
+    "website": "https://github.com/OCA/l10n-romania",
+    "depends": ["l10n_ro_efactura"],
+    "data": [],
+    "installable": True,
+    "auto_install": True,
+    "license": "LGPL-3",
 }
