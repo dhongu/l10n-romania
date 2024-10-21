6e08b3af27daa25cd93434f8dc2b3ab0b629ed32
Dorin Hongu <dhongu@gmail.com>
2024-10-21 19:47:11 +0300

    fix

--- l10n_ro_cash_register/__manifest__.py
+++ l10n_ro_cash_register/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Romania - Cash Register",
-    "version": "15.0.1.0.1",
+    "version": "15.0.1.0.2",
     "author": "Terrabit," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Romania Adaptation",
