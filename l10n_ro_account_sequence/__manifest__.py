4d91c0f424f37f9d576a3bedacec3b346467df4b
Dorin Hongu <dhongu@gmail.com>
2024-10-23 07:22:29 +0300

    generare de secventa pentru operatiile de casa

--- l10n_ro_account_sequence/__manifest__.py
+++ l10n_ro_account_sequence/__manifest__.py
@@ -1,12 +1,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Romania - Cash Sequence",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "author": "Terrabit," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Romania Adaptation",
     "depends": ["account"],
     "countries": ["ro"],
-    "data": [],
+    "data": [
+        "views/account_payment_view.xml",
+    ],
     "license": "AGPL-3",
 }
