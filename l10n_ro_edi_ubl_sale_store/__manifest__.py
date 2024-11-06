d7e3711b3f2cadec9d88682fab53d537992a0f26
Dorin Hongu <dhongu@gmail.com>
2024-11-06 07:53:20 +0200

    Sale from Store UBL

---
+++ l10n_ro_edi_ubl_sale_store/__manifest__.py
@@ -0,0 +1,24 @@
+# ©  2024 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Sale from Store UBL",
+    "version": "17.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "summary": "Sale from store",
+    "category": "Generic Modules",
+    "depends": [
+        "deltatech_sale_store",
+        "l10n_ro_edi",
+    ],
+    "license": "OPL-1",
+    "price": 25.00,
+    "currency": "EUR",
+    "data": [],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+    "extra_buy": True,
+}
