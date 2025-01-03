7caf5420f77bd40220763266c310995486735f62
Dorin Hongu <dhongu@gmail.com>
2025-01-03 08:16:21 +0200

    Fix l10n_ro_cash_document_type assignment logic.
    Corrected the conditional logic for assigning `l10n_ro_cash_document_type` based on payment type. Ensured proper evaluation of `partner_type` to match the expected outcomes for payment categorization. This change prevents incorrect type assignments during record creation.

--- l10n_ro_account_sequence/models/account_payment.py
+++ l10n_ro_account_sequence/models/account_payment.py
@@ -56,13 +56,15 @@ class AccountPayment(models.Model):
     def create(self, vals_list):
         for vals in vals_list:
             if not vals.get("l10n_ro_cash_document_type", False):
-                payment_type = vals.get("payment_type", 'inbound')
+                payment_type = vals.get("payment_type", "inbound")
                 if payment_type == "inbound":
                     vals["l10n_ro_cash_document_type"] = (
-                        "customer_receipt" if vals["partner_type"] == "customer" else "cash_collection"
+                        "customer_receipt" if payment_type == "customer" else "cash_collection"
                     )
+
                 elif payment_type == "outbound":
                     vals["l10n_ro_cash_document_type"] = (
-                        "supplier_receipt" if vals["partner_type"] == "supplier" else "payment_disposal"
+                        "supplier_receipt" if payment_type == "supplier" else "payment_disposal"
                     )
+
         return super().create(vals_list)
