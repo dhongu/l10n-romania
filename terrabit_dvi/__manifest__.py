c179f707c62bb398242e2b363ffe1ad13f3fd5de
Dorin Hongu <dhongu@gmail.com>
2025-01-26 12:25:29 +0200

    Add initial implementation for DVI integration module
    Included the base structure for the DVI (Import Customs Declaration) functionality in the Romania localization. This includes models, actions, and descriptions to manage VAT and reconciliations tied to landed costs with DVI entries.

---
+++ terrabit_dvi/__manifest__.py
@@ -0,0 +1,26 @@
+# ©  2008-2020 Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "Romania - DVI",
+    "license": "AGPL-3",
+    "version": "18.0.1.0.0",
+    "countries": ["ro"],
+    "author": "Terrabit," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "category": "Localization",
+    "depends": [
+        "stock_account",
+        "account",
+        "sale",
+        "l10n_ro",  # pentru determinare de conturi 446. 447
+        "purchase_stock",
+        "stock_landed_costs",
+    ],
+    "data": [
+        "views/account_invoice_view.xml",
+        "views/stock_landed_cost_view.xml",
+        "wizard/account_dvi_view.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+}
