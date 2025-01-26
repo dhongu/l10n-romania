88dc98c4a3cc91b7fd5191b3e4d74816ce58a60e
Dorin Hongu <dhongu@gmail.com>
2025-01-26 12:36:14 +0200

    Update Romanian localization modules with enhancements and fixes
    This commit introduces new features, improvements, and fixes across multiple Romanian localization modules. Highlights include a new computed warning field in partner creation, improved cash register balance computations, enhanced stock picking report layouts, and refactoring in EDI check models. Adjustments to various views, models, and access rights were made to optimize functionality and maintain consistency.

---
+++ l10n_ro_invoice_report/__manifest__.py
@@ -0,0 +1,33 @@
+# ©  2008-2020 Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Romania - Invoice Report Terrabit",
+    "summary": "Localizare Terrabit - Facturi, Chitanta",
+    "version": "18.0.3.4.9",
+    "author": "Dorin Hongu," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "license": "AGPL-3",
+    "category": "Localization",
+    "countries": ["ro"],
+    "depends": [
+        "base",
+        "account",
+        "l10n_ro_config",
+        "purchase",
+        "sale",
+        "stock_delivery",
+    ],
+    "data": [
+        "views/invoice_report.xml",
+        "views/voucher_report.xml",
+        "views/payment_report.xml",
+        "views/account_invoice_view.xml",
+        "views/account_voucher_report.xml",
+        "views/account_bank_statement_view.xml",
+        # "views/statement_report.xml",
+        "views/res_config_view.xml",
+        "views/account_journal_view.xml",
+        "views/res_partner_view.xml",
+    ],
+}
---
+++ l10n_ro_invoice_report/models/res_partner.py
@@ -0,0 +1,19 @@
+# ©  2008-2020 Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    info_for_invoice = fields.Html(string="Additional info for invoice")
+    mean_transp = fields.Char(string="Mean transport")
+    payment_bank_id = fields.Many2one(
+        "res.partner.bank",
+        domain="['|', ('company_id', '=', company_id), ('company_id', '=', False)]",
+        # company_dependent = True,
+        help="The bank account in which this partner will pay."
+        "Will be sent in the SPV and will be printed on the invoice",
+    )
