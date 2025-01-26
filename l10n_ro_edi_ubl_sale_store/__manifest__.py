fb7bf7ce7234dccbd17a464d270ab2efc1e1200a
Dorin Hongu <dhongu@gmail.com>
2025-01-26 12:21:11 +0200

    Add module l10n_ro_edi_ubl_sale_store
    Introduce a new Romanian localization module "Deltatech Sale from Store UBL" to handle specific sale invoice types. Includes models, README, static resources, and documentation for managing e-invoices with document type code 751 for printed receipts.

---
+++ l10n_ro_edi_ubl_sale_store/__manifest__.py
@@ -0,0 +1,26 @@
+# ©  2024 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Sale from Store UBL",
+    "version": "18.0.1.0.2",
+    "author": "Terrabit, Dorin Hongu, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "summary": "Sale from store",
+    "countries": ["ro"],
+    "category": "Localization",
+    "depends": [
+        "deltatech_sale_store",
+        "l10n_ro_edi",
+        "l10n_ro_efactura",
+    ],
+    "license": "LGPL-3",
+    "price": 25.00,
+    "currency": "EUR",
+    "data": [],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+    "extra_buy": True,
+}
