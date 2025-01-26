798eedf4236a53a86806e65ecce1c9dff32f3102
Dorin Hongu <dhongu@gmail.com>
2025-01-26 12:20:13 +0200

    Add new Romanian localization module for product expiry in picking reports
    This module extends the picking reports in Romania localization to include product expiry dates. It introduces new views, translations, documentation, and dependencies to ensure proper integration with related modules and functionalities.

---
+++ l10n_ro_stock_picking_report_product_expiry/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2008-2022 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Romania - Picking Reports -Product Expiry",
+    "license": "AGPL-3",
+    "version": "18.0.1.0.1",
+    "author": "Dorin Hongu," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "category": "Localization",
+    "countries": ["ro"],
+    "depends": ["l10n_ro_stock_picking_report", "product_expiry"],
+    "data": [
+        "views/report_picking.xml",
+    ],
+    "auto_install": True,
+}
