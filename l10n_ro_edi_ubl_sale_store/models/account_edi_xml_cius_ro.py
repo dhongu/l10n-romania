011f7c0e8faa87e417449f03108b86f750637e92
Dorin Hongu <dhongu@gmail.com>
2025-01-26 12:21:11 +0200

    Add module l10n_ro_edi_ubl_sale_store
    Introduce a new Romanian localization module "Deltatech Sale from Store UBL" to handle specific sale invoice types. Includes models, README, static resources, and documentation for managing e-invoices with document type code 751 for printed receipts.

---
+++ l10n_ro_edi_ubl_sale_store/models/account_edi_xml_cius_ro.py
@@ -0,0 +1,16 @@
+# ©  2024 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo import models
+
+
+class AccountEdiXmlUBLRO(models.AbstractModel):
+    _inherit = "account.edi.xml.ubl_ro"
+
+    def _export_invoice_vals(self, invoice):
+        vals_list = super()._export_invoice_vals(invoice)
+        if invoice.receipt_print:
+            vals_list["vals"]["document_type_code"] = 751
+        return vals_list
