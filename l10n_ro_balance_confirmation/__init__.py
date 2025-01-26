957ce3125a66d3f2d4e2f33f1e7573a4a6e49dc4
Dorin Hongu <dhongu@gmail.com>
2025-01-26 12:20:07 +0200

    Add l10n_ro_balance_confirmation module to Romania localization
    Introduce the balance confirmation feature for Romania, including models, reports, translations, and associated views. This module provides functionality to generate and manage balance confirmation forms based on partner data.

---
+++ l10n_ro_balance_confirmation/__init__.py
@@ -0,0 +1,6 @@
+# ©  2008-2022 Terrabit
+# See README.rst file on addons root folder for license details
+
+from . import wizard
+from . import report
+from . import models
---
+++ l10n_ro_balance_confirmation/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+Generare formular de confirmare de sold
