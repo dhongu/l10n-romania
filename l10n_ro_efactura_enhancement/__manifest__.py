3481b02ca0b55bff0998f3f9ecf4c46bb2de97a8
Dorin Hongu <dhongu@gmail.com>
2025-01-26 12:22:56 +0200

    Add eFactura enhancement module for l10n Romania
    This commit introduces a new module to enhance eFactura automation for Romanian localization. It includes new models, wizards, and cron functionality for automated invoicing, along with UI improvements and necessary assets. The enhancements focus on compliance with Romanian invoicing standards and process efficiency.

---
+++ l10n_ro_efactura_enhancement/__manifest__.py
@@ -0,0 +1,28 @@
+# ©  2024 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "eFactura Enhacement",
+    "version": "18.0.0.0.2",
+    "author": "Terrabit, Dorin Hongu, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "summary": "eFactura Enhacement",
+    "countries": ["ro"],
+    "category": "Localization",
+    "depends": [
+        "l10n_ro_edi",
+        "l10n_ro_efactura",
+    ],
+    "license": "LGPL-3",
+    "price": 25.00,
+    "currency": "EUR",
+    "data": [
+        "wizard/account_move_send_views.xml",
+        "data/ir_cron.xml",
+    ],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+    "extra_buy": True,
+}
---
+++ l10n_ro_efactura_enhancement/models/account_edi_xml_cius_ro.py
@@ -0,0 +1,86 @@
+# ©  2024 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo import models
+
+
+class AccountEdiXmlUBLRO(models.AbstractModel):
+    _inherit = "account.edi.xml.ubl_ro"
+
+    def _get_partner_party_vals(self, partner, role):
+        # EXTENDS account.edi.xml.ubl_21
+        vals = super()._get_partner_party_vals(partner, role)
+
+        partner = partner.commercial_partner_id
+
+        postal_address = vals.get("postal_address_vals", {})
+        if not postal_address.get("street_name", False):
+            postal_address["street_name"] = "Principala"
+
+        if postal_address.get("country_subentity", False) == "RO-B":
+            if "SECTOR" not in postal_address.get("city_name", "").upper():
+                postal_code = postal_address.get("postal_zone", False)
+                if postal_code and postal_code[0] == "0" and postal_code[1] in ["1", "2", "3", "4", "5", "6"]:
+                    postal_address["city_name"] = "SECTOR" + postal_code[1]
+                else:
+                    postal_address["city_name"] = "SECTOR1"
+
+        if not partner.is_company:
+            vals["endpoint_id"] = "0000000000000"
+        return vals
+
+    def _get_partner_party_tax_scheme_vals_list(self, partner, role):
+        # EXTENDS account.edi.xml.ubl_21
+        vals_list = super()._get_partner_party_tax_scheme_vals_list(partner, role)
+        partner = partner.commercial_partner_id
+        for vals in vals_list:
+            if not partner.is_company:
+                vals["company_id"] = "0000000000000"
+        return vals_list
+
+    def _get_partner_party_legal_entity_vals_list(self, partner):
+        val_list = super()._get_partner_party_legal_entity_vals_list(partner)
+        partner = partner.commercial_partner_id
+        if not partner.is_company:
+            for vals in val_list:
+                if vals.get("commercial_partner") == partner:
+                    vals["company_id"] = "0000000000000"
+        return val_list
+
+    def _get_invoice_line_item_vals(self, line, taxes_vals):
+        vals = super()._get_invoice_line_item_vals(line, taxes_vals)
+        name = vals.get("name") or "n/a"
+        vals["name"] = name[:100]
+        description = vals.get("description") or vals["name"]
+        vals["description"] = description[:200]
+
+        return vals
+
+    def _export_invoice_vals(self, invoice):
+        vals_list = super()._export_invoice_vals(invoice)
+
+        return vals_list
+
+    def _export_invoice_constraints(self, invoice, vals):
+        partner = invoice.commercial_partner_id
+
+        if partner.country_id.code == "RO" and not partner.is_company:
+            # if not partner.vat:
+            #     partner.with_context(no_vat_validation=True).write({"vat": "0000000000000"})
+            if not partner.street:
+                partner.write({"street": "Principala"})
+
+            if partner.state_id and partner.state_id.code == "B":
+                if not partner.city:
+                    partner.write({"city": "SECTOR1"})
+                if "SECTOR" not in partner.city.upper():
+                    partner.write({"city": "SECTOR1"})
+
+        constraints = super()._export_invoice_constraints(invoice, vals)
+
+        if not partner.is_company:
+            constraints.pop("ciusro_customer_tax_identifier_required", False)
+
+        return constraints
