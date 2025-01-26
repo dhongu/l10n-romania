52f3956b4dbabd8c9bca0a49a9fd42b587a5da8b
Dorin Hongu <dhongu@gmail.com>
2025-01-26 12:25:03 +0200

    Add Romania Stock Accounting Check module
    Introduces the `l10n_ro_stock_account_check` module for stock valuation correction and related functionalities. Includes server actions, views, and models to handle stock move corrections, revaluation, and adjustments tailored for the Romanian localization.

---
+++ l10n_ro_stock_account_check/__manifest__.py
@@ -0,0 +1,24 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Romania - Stock Accounting Check",
+    "license": "AGPL-3",
+    "version": "18.0.1.0.7",
+    "countries": ["ro"],
+    "author": "Terrabit,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "category": "Localization",
+    "depends": [
+        "l10n_ro_stock_account",
+        "purchase_stock",
+        "sale_stock",
+        "deltatech_purchase_price",
+    ],
+    "data": [
+        "report/stock_check_report_view.xml",
+        "views/stock_valuation_layer_view.xml",
+        "security/ir.model.access.csv",
+        "views/stock_view.xml",
+    ],
+    "installable": True,
+    "maintainers": ["dhongu"],
+}
