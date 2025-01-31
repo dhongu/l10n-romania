9224147d02aacd62cabe8b11033f730eb0a6da41
Dorin Hongu <dhongu@gmail.com>
2025-01-31 10:05:33 +0200

    Update Romanian localization modules with code improvements
    Revised the category in the manifest for better classification, adjusted the cash register operation to separate record creation and field update, and included the wizard module in the `__init__.py`. These changes improve clarity, maintainability, and functionality of the modules.

--- l10n_ro_account_sequence/__manifest__.py
+++ l10n_ro_account_sequence/__manifest__.py
@@ -4,7 +4,7 @@
     "version": "18.0.1.0.2",
     "author": "Terrabit," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
-    "category": "Romania Adaptation",
+    "category": "Localization",
     "depends": ["account"],
     "countries": ["ro"],
     "data": [
