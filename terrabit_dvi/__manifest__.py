8633ed0115ca9dbb93f64cfbb60ede7778838021
Dorin Hongu <dhongu@gmail.com>
2025-02-26 17:04:20 +0200

    Actualizează vizibilitatea câmpurilor și numele modulului
    Vizibilitatea grupului din fereastra landed cost este condiționată de tipul 'dvi'. Modificarea numelui modulului din "Romania - DVI" în "Terrabit - DVI" reflectă o mai bună identificare a specificațiilor sale.

--- terrabit_dvi/__manifest__.py
+++ terrabit_dvi/__manifest__.py
@@ -1,7 +1,7 @@
 # ©  2008-2020 Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 {
-    "name": "Romania - DVI",
+    "name": "Terrabit - DVI",
     "license": "AGPL-3",
     "version": "17.0.1.0.0",
     "countries": ["ro"],
