d44f62ea637a9c90d2e6d15270e596a9bae574e7
Dorin Hongu <dhongu@gmail.com>
2025-03-31 20:53:59 +0300

    Adaugă suport pentru trimiterea facturilor prin SPV
    Implementat `_request_ciusro_send_invoice` pentru a upload-a facturi în SPV folosind endpoint-uri specifice. Actualizat `__init__.py` pentru a importa noul modul.

---
+++ l10n_ro_efactura_enhancement/models/ciusro_document.py
@@ -0,0 +1,51 @@
+
+from odoo.addons.l10n_ro_efactura.models.ciusro_document import make_efactura_request, NS_UPLOAD
+from odoo import api, models
+import requests
+from lxml import etree
+
+class L10nRoEdiDocument(models.Model):
+    _inherit = 'l10n_ro_edi.document'
+
+
+
+
+
+    @api.model
+    def _request_ciusro_send_invoice(self, company, xml_data, move_type='out_invoice'):
+        """
+        This method makes an 'upload' request to send xml_data to Romanian SPV.Based on the result, it will then process
+        the answer and return a dictionary, which may consist of either an 'error' or a 'key_loading' string.
+
+        :param company: ``res.company`` object
+        :param xml_data: String of XML data to be sent
+        :param move_type: ``move_type`` field from ``account.move`` object, used for the request parameter
+        :return: Result dictionary -> {'error': <str>} | {'key_loading': <str>}
+        """
+
+
+        if self.invoice_id.commercial_partner_id.is_company:
+            endpoint = 'upload'
+        else:
+            endpoint = 'uploadb2c'
+
+        result = make_efactura_request(
+            session=requests,
+            company=company,
+            endpoint=endpoint,
+            method='POST',
+            params={'standard': 'UBL' if move_type == 'out_invoice' else 'CN',
+                    'cif': company.vat.replace('RO', '')},
+            data=xml_data,
+        )
+        if 'error' in result:
+            return result
+
+        root = etree.fromstring(result['content'])
+        res_status = root.get('ExecutionStatus')
+        if res_status == '1':
+            error_elements = root.findall('.//ns:Errors', namespaces=NS_UPLOAD)
+            error_messages = [error_element.get('errorMessage') for error_element in error_elements]
+            return {'error': '\n'.join(error_messages)}
+        else:
+            return {'key_loading': root.get('index_incarcare')}
