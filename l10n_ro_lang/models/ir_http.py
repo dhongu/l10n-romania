a23af4a0000689e6f72a0847e0e4965be4183715
Dorin Hongu <dhongu@gmail.com>
2025-04-15 12:21:03 +0300

    Corectează definiția metodei `get_nearest_lang` în `IrHttp`.
    Metoda `get_nearest_lang` trece de la utilizarea decoratoarelor de clasă la cele de model pentru a asigura compatibilitatea cu API-ul Odoo. De asemenea, s-a remediat o problemă minoră în `_inherit`.

--- l10n_ro_lang/models/ir_http.py
+++ l10n_ro_lang/models/ir_http.py
@@ -1,16 +1,16 @@
 import logging
 
-from odoo import models
+from odoo import api, models
 from odoo.http import request
 
 _logger = logging.getLogger(__name__)
 
 
 class IrHttp(models.AbstractModel):
-    _inherit = ["ir.http"]
+    _inherit = "ir.http"
 
-    @classmethod
-    def get_nearest_lang(cls, lang_code: str) -> str:
+    @api.model
+    def get_nearest_lang(self, lang_code: str) -> str:
         res = super().get_nearest_lang(lang_code)
         country_code = request.geoip.country_code
         if country_code == "RO" and not lang_code:
