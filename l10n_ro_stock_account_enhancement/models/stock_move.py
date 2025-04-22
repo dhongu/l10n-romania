1235f76f7f6c4ac34114a0b8ec9ba75e4698a4b5
Dorin Hongu <dhongu@gmail.com>
2025-04-22 07:39:36 +0300

    Adaugă informații de licențiere în stock_move.py
    Adaugă detalii despre drepturile de autor și licențiere pentru fișierul `stock_move.py`. Această modificare îmbunătățește conformitatea cu politicile proiectului și claritatea.

--- l10n_ro_stock_account_enhancement/models/stock_move.py
+++ l10n_ro_stock_account_enhancement/models/stock_move.py
@@ -1,3 +1,7 @@
+# © 2025 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
 from odoo import _, models
 from odoo.exceptions import UserError
 
