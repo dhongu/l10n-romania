4518cf6be46d0173e178afd8f803995307af5200
Dorin Hongu <dhongu@gmail.com>
2025-05-09 04:57:17 +0300

    Adaugă modulul eTransport enhancement
    Adaugă un modul nou pentru extensia funcționalităților eTransport, inclusiv gestionarea transferurilor și îmbunătățiri pentru campurile și stările asociate. Include fișiere de configurație, inițializare, șabloane și documentație.

---
+++ l10n_ro_etransport_enhancement/models/stock_picking.py
@@ -0,0 +1,45 @@
+# ©  2025 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import api, fields, models
+
+
+class Picking(models.Model):
+    _inherit = "stock.picking"
+
+    l10n_ro_edi_stock_required = fields.Boolean(string="eTransport Required")
+
+    def _compute_l10n_ro_edi_stock_enable(self):
+        res = super()._compute_l10n_ro_edi_stock_enable()
+        for picking in self:
+            picking.l10n_ro_edi_stock_enable = picking.l10n_ro_edi_stock_required
+        return res
+
+    @api.depends("l10n_ro_edi_stock_enable", "state", "l10n_ro_edi_stock_state")
+    def _compute_l10n_ro_edi_stock_enable_send(self):
+        res = super()._compute_l10n_ro_edi_stock_enable_send()
+        for picking in self:
+            picking.l10n_ro_edi_stock_enable_send = (
+                picking.l10n_ro_edi_stock_enable
+                and picking.l10n_ro_edi_stock_state in (False, "stock_sending_failed")
+                and not picking._l10n_ro_edi_stock_get_last_document("stock_validated")
+            )
+        return res
+
+    @api.model
+    def _l10n_ro_edi_stock_get_template_data(self, data: dict):
+        res = super()._l10n_ro_edi_stock_get_template_data(data)
+        for key in ("locStartTraseuRutier", "locFinalTraseuRutier"):
+            locatie = res["data"]["notificare"][key].get("locatie", {})
+            if locatie and not locatie["alteInfo"]:
+                locatie["alteInfo"] = "-"
+        return res
+
+    def action_l10n_ro_edi_stock_fetch_status(self):
+        res = super().action_l10n_ro_edi_stock_fetch_status()
+        for picking in self:
+            if picking.l10n_ro_edi_stock_state == "stock_validated":
+                picking.carrier_tracking_ref = picking.l10n_ro_edi_stock_document_uit
+
+        return res
