43fec034385522343084d96727f8818f03a44c1a
Dorin Hongu <dhongu@gmail.com>
2025-05-14 19:36:38 +0300

    Adaugă modulul "l10n_ro_account_storno" pentru îmbunătățiri storno
    Adaugă un nou modul pentru gestionarea linilor contabile storno în conformitate cu cerințele din România. Include modele, vizualizări, documentație și configurări specifice pentru utilizarea conturilor storno.

---
+++ l10n_ro_account_storno/__manifest__.py
@@ -0,0 +1,16 @@
+# ©  2025 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Romania - Storno Enhancements",
+    "version": "17.0.0.0.1",
+    "author": "Dorin Hongu," "Terrabit," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "category": "Localization",
+    "depends": ["account"],
+    "countries": ["ro"],
+    "data": ["views/account_account_view.xml"],
+    "license": "AGPL-3",
+    "maintainers": ["dhongu"],
+}
