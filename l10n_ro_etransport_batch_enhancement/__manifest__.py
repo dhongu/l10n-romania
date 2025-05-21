fe1e774c6a58c4cb835d08cf53c793f60e55a7c7
Dorin Hongu <dhongu@gmail.com>
2025-05-21 12:06:00 +0300

    Adaugă îmbunătățiri eTransport și eFactura.
    Acest commit introduce noul modul "eTransport Batch Enhancement" pentru România, care aduce capabilități suplimentare în gestionarea transporturilor. De asemenea, elimină opțiuni redundante în gestionarea eFactura și optimizează procesarea automată a facturilor emise.

---
+++ l10n_ro_etransport_batch_enhancement/__manifest__.py
@@ -0,0 +1,29 @@
+# ©  2025 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "eTransport Batch Enhancement",
+    "version": "18.0.0.0.1",
+    "author": "Terrabit, Dorin Hongu, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "summary": "eTransport Batch Enhancement",
+    "countries": ["ro"],
+    "category": "Localization",
+    "depends": [
+        "l10n_ro_edi",
+        "l10n_ro_edi_stock",
+        "l10n_ro_edi_stock_batch",
+        "l10n_ro_etransport_enhancement",
+    ],
+    "license": "LGPL-3",
+    "price": 25.00,
+    "currency": "EUR",
+    "data": [
+        "views/stock_picking_batch_view.xml",
+    ],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+    "extra_buy": True,
+}
---
+++ l10n_ro_etransport_batch_enhancement/models/stock_picking_batch.py
@@ -0,0 +1,60 @@
+# ©  2025 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import _, api, fields, models
+
+
+class StockPickingBatch(models.Model):
+    _inherit = "stock.picking.batch"
+
+    l10n_ro_edi_stock_required = fields.Boolean(string="eTransport Required")
+
+    def _compute_l10n_ro_edi_stock_enable(self):
+        res = super()._compute_l10n_ro_edi_stock_enable()
+        for picking in self:
+            picking.l10n_ro_edi_stock_enable = picking.l10n_ro_edi_stock_required
+        return res
+
+    @api.depends("l10n_ro_edi_stock_enable", "state", "l10n_ro_edi_stock_state")
+    def _compute_l10n_ro_edi_stock_enable_send(self):
+        res = super()._compute_l10n_ro_edi_stock_enable_send()
+        for picking in self:
+            picking.l10n_ro_edi_stock_enable_send = (
+                picking.l10n_ro_edi_stock_enable
+                and picking.l10n_ro_edi_stock_state in (False, "stock_sending_failed")
+                and not picking._l10n_ro_edi_stock_get_last_document("stock_validated")
+            )
+        return res
+
+    @api.model
+    def _l10n_ro_edi_stock_get_template_data(self, data: dict):
+        res = super()._l10n_ro_edi_stock_get_template_data(data)
+        for key in ("locStartTraseuRutier", "locFinalTraseuRutier"):
+            locatie = res["data"]["notificare"][key].get("locatie", {})
+            if locatie and not locatie["alteInfo"]:
+                locatie["alteInfo"] = "-"
+        transport_partner = data["transport_partner_id"]
+        if transport_partner.country_code == "GR":
+            res["data"]["notificare"]["dateTransport"]["codTaraOrgTransport"] = "EL"
+        if res["data"]["notificare"]["partenerComercial"]["codTara"] == "GR":
+            res["data"]["notificare"]["partenerComercial"]["codTara"] = "EL"
+        return res
+
+    def action_l10n_ro_edi_stock_fetch_status(self):
+        res = super().action_l10n_ro_edi_stock_fetch_status()
+        for picking in self:
+            if picking.l10n_ro_edi_stock_state == "stock_validated":
+                picking.carrier_tracking_ref = picking.l10n_ro_edi_stock_document_uit
+
+        return res
+
+    @api.model
+    def _l10n_ro_edi_stock_validate_data(self, data: dict):
+        errors = super()._l10n_ro_edi_stock_validate_data(data)
+
+        for error in errors:
+            if error == _("The delivery carrier partner has to be located in Romania."):
+                errors.remove(error)
+
+        return errors
