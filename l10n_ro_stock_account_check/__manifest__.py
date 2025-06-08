2a151dabe25252959f8b3e8828cdad9f8e5fd50a
Dorin Hongu <dhongu@gmail.com>
2025-06-08 12:59:57 +0300

    Adaugă câmpuri și logică suplimentară pentru raportarea stocurilor
    Adaugă câmpurile `remaining_qty` și `remaining_value` în raportul de verificare a stocurilor. Actualizează logica de calcul pentru a include aceste noi câmpuri și îmbunătățește validarea datelor pentru o gestionare mai precisă a stocurilor.

--- l10n_ro_stock_account_check/__manifest__.py
+++ l10n_ro_stock_account_check/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Romania - Stock Accounting Check",
     "license": "AGPL-3",
-    "version": "17.0.1.0.7",
+    "version": "17.0.1.0.8",
     "countries": ["ro"],
     "author": "Terrabit,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
