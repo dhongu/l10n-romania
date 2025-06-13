355402f4b1b91a35a55f235405746a7ef7601750
Dorin Hongu <dhongu@gmail.com>
2025-06-13 11:18:09 +0300

    Comentează validarea prețurilor standard în metoda `write`
    Dezactivează temporar condiția care interzice salvarea prețurilor standard mai mici sau egale cu zero. Această schimbare permite mai multă flexibilitate, necesitând eventual reactivarea validării în viitor.

--- l10n_ro_stock_account_enhancement/models/product_product.py
+++ l10n_ro_stock_account_enhancement/models/product_product.py
@@ -7,6 +7,6 @@ class ProductProduct(models.Model):
     def write(self, vals):
         if "standard_price" in vals:
             self = self.with_context(disable_auto_svl=True)
-            if vals["standard_price"] <= 0:
-                raise ValueError("Standard price must be greater than zero.")
+            # if vals["standard_price"] <= 0:
+            #     raise ValueError("Standard price must be greater than zero.")
         return super().write(vals)
