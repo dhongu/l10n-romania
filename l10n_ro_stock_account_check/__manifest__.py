d2fe605f799ec1d323c96995e3d9f31042439e98
Dorin Hongu <dhongu@gmail.com>
2025-06-16 08:22:23 +0300

    Actualizează versiunea și adaugă decorații pentru câmpuri
    Incrementarea versiunii modulului la 17.0.1.1.2 reflectă modificările recente. Adaugă o decorație suplimentară `decoration-success` pentru câmpul `remaining_value` în raport, îmbunătățind claritatea vizuală și evidențierea datelor corecte.

--- l10n_ro_stock_account_check/__manifest__.py
+++ l10n_ro_stock_account_check/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Romania - Stock Accounting Check",
     "license": "AGPL-3",
-    "version": "17.0.1.1.1",
+    "version": "17.0.1.1.2",
     "countries": ["ro"],
     "author": "Terrabit,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
