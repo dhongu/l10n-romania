04f3e9e2ac2628bb1143dfa42530892873a1583f
Dorin Hongu <dhongu@gmail.com>
2025-07-10 15:51:55 +0300

    Elimină context-ul inutil din butonul „Get Data”
    A fost șters atributul `context` din butonul „Get Data” pentru simplificarea codului, deoarece acesta nu avea niciun impact asupra funcționalității. Incrementată versiunea modulului la 18.0.1.1.5 pentru reflectarea modificării.

--- l10n_ro_partner_create_by_vat_button/__manifest__.py
+++ l10n_ro_partner_create_by_vat_button/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Romania - Partner Create by VAT Button",
     "license": "AGPL-3",
-    "version": "18.0.1.1.4",
+    "version": "18.0.1.1.5",
     "author": "Dorin Hongu," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Localization",
