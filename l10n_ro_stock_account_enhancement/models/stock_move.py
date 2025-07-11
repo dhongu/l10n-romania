32590b5947eb04a92ddd69106364748d331183c1
Dorin Hongu <dhongu@gmail.com>
2025-07-11 06:00:45 +0300

    Adaugă opțiune pentru a omite verificarea prețului unitar
    Integrează o verificare opțională controlată prin parametrul `l10n_ro_stock_account.skip_price_unit_check` din sistemul de configurație. Această modificare permite flexibilitate în scenariile unde prețul unitar nu este obligatoriu, îmbunătățind adaptabilitatea modulului.

--- l10n_ro_stock_account_enhancement/models/stock_move.py
+++ l10n_ro_stock_account_enhancement/models/stock_move.py
@@ -4,6 +4,7 @@
 
 from odoo import _, models
 from odoo.exceptions import UserError
+from odoo.tools.convert import safe_eval
 
 
 class StockMove(models.Model):
@@ -11,7 +12,11 @@ class StockMove(models.Model):
 
     def _get_price_unit(self):
         price = super()._get_price_unit()
-
+        get_param = self.env["ir.config_parameter"].sudo().get_param
+        skip_price_unit_check = get_param("l10n_ro_stock_account.skip_price_unit_check", default=False)
+        skip_price_unit_check = safe_eval(skip_price_unit_check)
+        if skip_price_unit_check:
+            return price
         if not price:
             product = self.product_id
             raise UserError(_(f"Price unit is not set for product {product.name}"))
