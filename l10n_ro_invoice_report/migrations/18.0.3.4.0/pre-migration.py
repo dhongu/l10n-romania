bc55a03464af95f095ba3c9d6d586c7a801e82f3
Dorin Hongu <dhongu@gmail.com>
2025-08-15 07:38:00 +0300

    Comentează scriptul de pre-migrare inutil din `l10n_ro_invoice_report`.
    Codul destinat migrării pre-script a fost dezactivat prin comentare, deoarece nu mai are relevanță în versiunea actuală. Această modificare simplifică codul și pregătește modulul pentru viitoare ajustări sau optimizări.

--- l10n_ro_invoice_report/migrations/18.0.3.4.0/pre-migration.py
+++ l10n_ro_invoice_report/migrations/18.0.3.4.0/pre-migration.py
@@ -3,40 +3,40 @@ import logging
 from odoo.upgrade import util
 
 _logger = logging.getLogger(__name__)
-
-
-def migrate(cr, version):
-    _logger.info("_____________ Migration pre-script  _____________")
-
-    env = util.env(cr)
-    companies = env["res.company"].sudo().search([])
-    for company in companies:
-        ext_id = f"account.{company.id}_tvac_19"
-        tva = env.ref(ext_id, raise_if_not_found=False)
-        if tva:
-            tva.invoice_label = "19%"
-
-        ext_id = f"account.{company.id}_tvac_19_s"
-        tva = env.ref(ext_id, raise_if_not_found=False)
-        if tva:
-            tva.invoice_label = "19%"
-
-        ext_id = f"account.{company.id}_tvac_09"
-        tva = env.ref(ext_id, raise_if_not_found=False)
-        if tva:
-            tva.invoice_label = "9%"
-
-        ext_id = f"account.{company.id}_tvac_09_s"
-        tva = env.ref(ext_id, raise_if_not_found=False)
-        if tva:
-            tva.invoice_label = "9%"
-
-        ext_id = f"account.{company.id}_tvac_05"
-        tva = env.ref(ext_id, raise_if_not_found=False)
-        if tva:
-            tva.invoice_label = "5%"
-
-        ext_id = f"account.{company.id}_tvac_05_s"
-        tva = env.ref(ext_id, raise_if_not_found=False)
-        if tva:
-            tva.invoice_label = "5%"
+#
+#
+# def migrate(cr, version):
+#     _logger.info("_____________ Migration pre-script  _____________")
+#
+#     env = util.env(cr)
+#     companies = env["res.company"].sudo().search([])
+#     for company in companies:
+#         ext_id = f"account.{company.id}_tvac_19"
+#         tva = env.ref(ext_id, raise_if_not_found=False)
+#         if tva:
+#             tva.invoice_label = "19%"
+#
+#         ext_id = f"account.{company.id}_tvac_19_s"
+#         tva = env.ref(ext_id, raise_if_not_found=False)
+#         if tva:
+#             tva.invoice_label = "19%"
+#
+#         ext_id = f"account.{company.id}_tvac_09"
+#         tva = env.ref(ext_id, raise_if_not_found=False)
+#         if tva:
+#             tva.invoice_label = "9%"
+#
+#         ext_id = f"account.{company.id}_tvac_09_s"
+#         tva = env.ref(ext_id, raise_if_not_found=False)
+#         if tva:
+#             tva.invoice_label = "9%"
+#
+#         ext_id = f"account.{company.id}_tvac_05"
+#         tva = env.ref(ext_id, raise_if_not_found=False)
+#         if tva:
+#             tva.invoice_label = "5%"
+#
+#         ext_id = f"account.{company.id}_tvac_05_s"
+#         tva = env.ref(ext_id, raise_if_not_found=False)
+#         if tva:
+#             tva.invoice_label = "5%"
