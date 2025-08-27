6cf37e71414ef82490186716298e017241bad0fe
Dorin Hongu <dhongu@gmail.com>
2025-08-27 14:15:34 +0300

    Adaugă modulul `l10n_ro_zip` pentru gestionarea codurilor poștale
    Introduce un nou modul pentru gestionarea codurilor poștale din România, incluzând un fișier SQL pentru popularea datelor în tabela `res_zip`. Modulul oferă funcționalități suplimentare de actualizare automată a câmpurilor stat, oraș și țară pe baza regulilor definite.

---
+++ l10n_ro_zip/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2020 Terrabit
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Romania - Coduri Postale",
+    "summary": "Romania - Coduri Postale",
+    "countries": ["ro"],
+    "license": "AGPL-3",
+    "version": "17.0.0.0.0",
+    "author": "Terrabit,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "category": "Localization",
+    "depends": ["base_address_extended", "l10n_ro_config"],
+    "data": [
+
+    ],
+    "development_status": "Beta",
+    "installable": True,
+    "maintainers": ["dhongu"],
+    "post_init_hook": "post_init_hook",
+
+}
