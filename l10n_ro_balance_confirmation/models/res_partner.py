74eb082c9fc9a23ea69536a14592de7303205dbc
Dorin Hongu <dhongu@gmail.com>
2025-08-29 16:37:15 +0300

    Optimizează metoda `_credit_debit_get` în `l10n_ro_balance_confirmation`.
    S-a actualizat logica metodei pentru a utiliza construcții SQL mai eficiente și simplificate. Această modificare îmbunătățește performanța și claritatea codului. De asemenea, s-a incrementat versiunea modulului pentru a reflecta schimbările.

--- l10n_ro_balance_confirmation/models/res_partner.py
+++ l10n_ro_balance_confirmation/models/res_partner.py
@@ -3,13 +3,16 @@
 
 
 from odoo import api, models
-
+from odoo.tools import SQL, unique
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
+
+
     @api.depends_context("company", "date_to")
     def _credit_debit_get(self):
+        """ Este functia standard la care am adauga in flitrare si datarea de la date_to"""
         date = self.env.context.get("date_to")
         if not date:
             return super()._credit_debit_get()
@@ -18,64 +21,42 @@ class ResPartner(models.Model):
             self.debit = False
             self.credit = False
             return
-        tables, where_clause, where_params = (
-            self.env["account.move.line"]
-            ._where_calc(
-                [
-                    ("parent_state", "=", "posted"),
-                    ("company_id", "child_of", self.env.company.root_id.id),
-                    ("date", "<=", date),
-                ]
-            )
-            .get_sql()
+        query = self.env['account.move.line']._where_calc([
+            ('parent_state', '=', 'posted'),
+            ('company_id', 'child_of', self.env.company.root_id.id),
+            ("date", "<=", date),
+        ])
+        self.env['account.move.line'].flush_model(
+            ['account_id', 'amount_residual', 'company_id', 'parent_state', 'partner_id', 'reconciled']
         )
-
-        where_params = [tuple(self.ids)] + where_params
-        if where_clause:
-            where_clause = "AND " + where_clause
-        self.env["account.move.line"].flush_model(
-            [
-                "account_id",
-                "debit",
-                "credit",
-                "amount_residual",
-                "company_id",
-                "parent_state",
-                "partner_id",
-                "reconciled",
-            ]
-        )
-        self.env["account.account"].flush_model(["account_type"])
-        self._cr.execute(
-            """
-            SELECT account_move_line.partner_id, a.account_type, SUM(account_move_line.debit - account_move_line.credit)
-                      FROM """
-            + tables
-            + """
-                      LEFT JOIN account_account a ON (account_move_line.account_id=a.id)
-                      WHERE a.account_type IN ('asset_receivable','liability_payable')
-                      AND account_move_line.partner_id IN %s
-
-                      """
-            + where_clause
-            + """
-                      GROUP BY account_move_line.partner_id, a.account_type
-                      """,
-            where_params,
+        self.env['account.account'].flush_model(['account_type'])
+        sql = SQL("""
+            SELECT account_move_line.partner_id, a.account_type, SUM(account_move_line.amount_residual)
+            FROM %s
+            LEFT JOIN account_account a ON (account_move_line.account_id=a.id)
+            WHERE a.account_type IN ('asset_receivable','liability_payable')
+            AND account_move_line.partner_id IN %s
+            AND account_move_line.reconciled IS NOT TRUE
+            AND %s
+            GROUP BY account_move_line.partner_id, a.account_type
+            """,
+            query.from_clause,
+            tuple(self.ids),
+            query.where_clause or SQL("TRUE"),
         )
         treated = self.browse()
-        for pid, acc_type, val in self._cr.fetchall():
+        for pid, account_type, val in self.env.execute_query(sql):
             partner = self.browse(pid)
-            if acc_type == "asset_receivable":
+            if account_type == 'asset_receivable':
                 partner.credit = val
                 if partner not in treated:
                     partner.debit = False
                     treated |= partner
-            elif acc_type == "liability_payable":
+            elif account_type == 'liability_payable':
                 partner.debit = -val
                 if partner not in treated:
                     partner.credit = False
                     treated |= partner
-        remaining = self - treated
+        remaining = (self - treated)
         remaining.debit = False
         remaining.credit = False
