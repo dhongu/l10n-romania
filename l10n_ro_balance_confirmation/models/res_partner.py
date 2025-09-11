932430f273fa0c5731fdf4d582d366d676fbf4fc
danila12 <danila@terrabit.ro>
2025-09-11 13:27:57 +0300

    added product/description lengths in invoice line, code cleaning

--- l10n_ro_balance_confirmation/models/res_partner.py
+++ l10n_ro_balance_confirmation/models/res_partner.py
@@ -3,16 +3,15 @@
 
 
 from odoo import api, models
-from odoo.tools import SQL, unique
+from odoo.tools import SQL
+
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-
-
     @api.depends_context("company", "date_to")
     def _credit_debit_get(self):
-        """ Este functia standard la care am adauga in flitrare si datarea de la date_to"""
+        """Este functia standard la care am adauga in flitrare si datarea de la date_to"""
         date = self.env.context.get("date_to")
         if not date:
             return super()._credit_debit_get()
@@ -21,16 +20,19 @@ class ResPartner(models.Model):
             self.debit = False
             self.credit = False
             return
-        query = self.env['account.move.line']._where_calc([
-            ('parent_state', '=', 'posted'),
-            ('company_id', 'child_of', self.env.company.root_id.id),
-            ("date", "<=", date),
-        ])
-        self.env['account.move.line'].flush_model(
-            ['account_id', 'amount_residual', 'company_id', 'parent_state', 'partner_id', 'reconciled']
+        query = self.env["account.move.line"]._where_calc(
+            [
+                ("parent_state", "=", "posted"),
+                ("company_id", "child_of", self.env.company.root_id.id),
+                ("date", "<=", date),
+            ]
         )
-        self.env['account.account'].flush_model(['account_type'])
-        sql = SQL("""
+        self.env["account.move.line"].flush_model(
+            ["account_id", "amount_residual", "company_id", "parent_state", "partner_id", "reconciled"]
+        )
+        self.env["account.account"].flush_model(["account_type"])
+        sql = SQL(
+            """
             SELECT account_move_line.partner_id, a.account_type, SUM(account_move_line.amount_residual)
             FROM %s
             LEFT JOIN account_account a ON (account_move_line.account_id=a.id)
@@ -47,16 +49,16 @@ class ResPartner(models.Model):
         treated = self.browse()
         for pid, account_type, val in self.env.execute_query(sql):
             partner = self.browse(pid)
-            if account_type == 'asset_receivable':
+            if account_type == "asset_receivable":
                 partner.credit = val
                 if partner not in treated:
                     partner.debit = False
                     treated |= partner
-            elif account_type == 'liability_payable':
+            elif account_type == "liability_payable":
                 partner.debit = -val
                 if partner not in treated:
                     partner.credit = False
                     treated |= partner
-        remaining = (self - treated)
+        remaining = self - treated
         remaining.debit = False
         remaining.credit = False
