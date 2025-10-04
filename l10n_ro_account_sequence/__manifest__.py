f4f59a43399580aa4f995606c2ef9f2ca28e7e3a
Dorin Hongu <dhongu@gmail.com>
2025-10-04 17:04:43 +0300

    [IMP] l10n_ro_account_sequence: actualizare manifest
    Actualizat fișierul manifest pentru a reflecta versiunea 19.0 și schimbările aferente. Adăugat câmpurile `summary` și `development_status` pentru o descriere mai detaliată și pentru a indica stadiul matur al modulului.

--- l10n_ro_account_sequence/__manifest__.py
+++ l10n_ro_account_sequence/__manifest__.py
@@ -1,8 +1,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Romania - Cash Sequence",
-    "version": "18.0.1.0.3",
+    "version": "19.0.1.0.3",
     "author": "Terrabit,Odoo Community Association (OCA)",
+    "summary": "Romania - Cash Sequence",
+    "development_status": "Mature",
     "website": "https://www.terrabit.ro",
     "category": "Localization",
     "depends": ["account"],
