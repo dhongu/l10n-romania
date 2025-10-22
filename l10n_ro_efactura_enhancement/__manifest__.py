0531e5447b4d36deb2262d6b75242df30ec3cd55
Dorin Hongu <dhongu@gmail.com>
2025-10-22 07:59:49 +0300

    Actualizează versiunea modulului la 18.0.0.0.12
    Modifică versiunea în manifestul modulului pentru a reflecta schimbările recente. Această actualizare este necesară pentru alinierea la convențiile de versiune și pentru a semnaliza noile îmbunătățiri.

--- l10n_ro_efactura_enhancement/__manifest__.py
+++ l10n_ro_efactura_enhancement/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "eFactura Enhancement",
-    "version": "18.0.0.0.11",
+    "version": "18.0.0.0.12",
     "author": "Terrabit, Dorin Hongu, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "summary": "eFactura Enhancement",
