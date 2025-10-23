abb2c919898910f91cfc29a7c1f409a52a783717
Dorin Hongu <dhongu@gmail.com>
2025-10-23 22:02:52 +0300

    Actualizează structura și stilul rapoartelor și versiunile modulelor
    Se modifică stilul tabelelor din rapoarte pentru o afișare mai curată și se corectează mici erori în nomenclatură. Actualizează versiunile modulelor în manifest pentru a reflecta aceste schimbări.

--- l10n_ro_stock_picking_report/__manifest__.py
+++ l10n_ro_stock_picking_report/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Romania - Terrabit - Picking Reports",
     "summary": "Rapoarte: NIR, aviz, bon consum",
     "license": "AGPL-3",
-    "version": "18.0.1.2.4",
+    "version": "18.0.1.2.5",
     "author": "Dorin Hongu," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Localization",
