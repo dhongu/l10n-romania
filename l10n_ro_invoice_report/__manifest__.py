73269d1722b14b7849729dc514475bd3ad664320
Dorin Hongu <dhongu@gmail.com>
2025-10-24 07:08:32 +0300

    Optimizează sintaxa XML în l10n_ro_invoice_report
    Eliminate spații inutile și optimizată formatarea în fișierul XML al modulului pentru o lizibilitate mai bună. Ajustările nu afectează funcționalitatea, dar îmbunătățesc consistența și claritatea codului.

--- l10n_ro_invoice_report/__manifest__.py
+++ l10n_ro_invoice_report/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Romania - Invoice Report Terrabit",
     "summary": "Localizare Terrabit - Facturi, Chitanta",
-    "version": "19.0.3.4.9",
+    "version": "19.0.3.4.14",
     "author": "Dorin Hongu,Terrabit,Odoo Community Association (OCA)",
     "website": "https://www.terrabit.ro",
     "license": "AGPL-3",
