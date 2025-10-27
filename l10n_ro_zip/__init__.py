625553d27beeac3f48a5b0784bd53fb8698ab6db
Dorin Hongu <dhongu@gmail.com>
2025-10-27 08:18:56 +0200

    Adaugă funcționalitatea `unaccent` pentru actualizările `city_id`
    Se adaugă utilizarea funcției `unaccent` în interogările SQL care leagă orașele de codurile poștale. Această modificare îmbunătățește acuratețea actualizărilor prin eliminarea diferențelor cauzate de caracterele diacritice.

--- l10n_ro_zip/__init__.py
+++ l10n_ro_zip/__init__.py
@@ -45,7 +45,7 @@ def post_init_hook(env):
     sql = """
           UPDATE res_zip rz
           SET city_id = rc.id FROM res_city rc
-          WHERE rz.city = rc.name->>'en_US'
+          WHERE unaccent(rz.city) = unaccent(rc.name->>'en_US')
             AND rz.state_id = rc.state_id
             AND rz.name = rc.zipcode
           """
@@ -55,7 +55,7 @@ def post_init_hook(env):
     sql = """
           UPDATE res_zip rz
           SET city_id = rc.id FROM res_city rc
-          WHERE rz.city = rc.name->>'en_US'
+          WHERE unaccent(rz.city) = unaccent(rc.name->>'en_US')
             AND rz.state_id = rc.state_id
             AND city_id is null
           """
