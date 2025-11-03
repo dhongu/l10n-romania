ffa1438313402f61e3cfbd48d0b384a7c2ea2cdf
Dorin Hongu <dhongu@gmail.com>
2025-11-03 13:40:20 +0200

    Dezactivează constrângerea `check_vat` în modelul `res.partner`
    Comentată logica din metoda `check_vat` pentru a dezactiva temporar verificarea CUI, posibil în scopul depanării sau al testării. Aceasta împiedică validarea automată a CUI pentru partenerii non-RO.

--- l10n_ro_partner_create_by_vat_button/models/res_partner.py
+++ l10n_ro_partner_create_by_vat_button/models/res_partner.py
@@ -37,12 +37,12 @@ class ResPartner(models.Model):
                 if missing:
                     partner.warning_message = self.env._("Missing: ") + ", ".join(missing)
 
-    @api.constrains("vat", "country_id")
-    def check_vat(self):
-        if self.env.context.get("no_vat_validation"):
-            return
-        partners = self.filtered(lambda p: p.country_id.code != "RO")
-        return super(ResPartner, partners).check_vat()
+    # @api.constrains("vat", "country_id")
+    # def check_vat(self):
+    #     if self.env.context.get("no_vat_validation"):
+    #         return
+    #     partners = self.filtered(lambda p: p.country_id.code != "RO")
+    #     return super(ResPartner, partners).check_vat()
 
     @api.model_create_multi
     def create(self, vals_list):
