8a8a0eb8aaf4534cf23851960b7f270e9f831804
Dorin Hongu <dhongu@gmail.com>
2025-11-06 13:55:44 +0200

    Adaugă câmpuri noi în modelul `account.move` și ajustează manifestul
    Se introduc câmpurile `only_fiscal_receipt` și `receipt_print` pentru gestionarea informațiilor legate de bonurile fiscale. De asemenea, se comentează dependența `deltatech_sale_store` din manifest, îmbunătățind întreținerea și claritatea codului.

--- l10n_ro_edi_ubl_sale_store/models/account_move.py
+++ l10n_ro_edi_ubl_sale_store/models/account_move.py
@@ -1,8 +1,11 @@
-from odoo import models
+from odoo import fields, models
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
+
+    only_fiscal_receipt = fields.Boolean()
+    receipt_print = fields.Boolean()  # bon fiscal tiparit
 
     def _need_ubl_cii_xml(self, ubl_cii_format):
         res = super()._need_ubl_cii_xml(ubl_cii_format)
