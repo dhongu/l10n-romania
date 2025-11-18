7518974d960b04236540ea2682eec4d8f6684934
Dorin Hongu <dhongu@gmail.com>
2025-11-18 19:34:07 +0200

    [ADD] l10n_ro_account_storno: teste pentru funcționalitatea storno
    Adaugă o nouă clasă de teste pentru a verifica comportamentul funcționalității storno în modulul de localizare contabilă din România. Include verificări pentru jurnale, conturi și mișcări contabile, asigurând conformitatea cu așteptările legale și tehnice locale. Actualizează manifestul pentru a reflecta noile teste și dependențe.

--- l10n_ro_account_storno/__manifest__.py
+++ l10n_ro_account_storno/__manifest__.py
@@ -4,13 +4,15 @@
 
 {
     "name": "Romania - Storno Enhancements",
+    "summary": "Romania - Storno Enhancements",
     "version": "19.0.0.0.1",
     "author": "Dorin Hongu,Terrabit,Odoo Community Association (OCA)",
     "website": "https://www.terrabit.ro",
     "category": "Localization",
-    "depends": ["account"],
+    "depends": ["account", "l10n_ro"],
     "countries": ["ro"],
     "data": ["views/account_account_view.xml"],
     "license": "AGPL-3",
     "maintainers": ["dhongu"],
+    "development_status": "Production/Stable",
 }
