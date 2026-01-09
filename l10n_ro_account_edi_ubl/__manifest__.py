988b94ee1154cbd4752d1f0753f2960f32faa71d
Dorin Hongu <dhongu@gmail.com>
2026-01-09 14:48:13 +0200

    [IMP] l10n_ro_account_edi_ubl: pre-commit stuff

--- l10n_ro_account_edi_ubl/__manifest__.py
+++ l10n_ro_account_edi_ubl/__manifest__.py
@@ -1,13 +1,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    "author": "Terrabit," "Odoo Community Association (OCA)",
+    "author": "Terrabit,Odoo Community Association (OCA)",
     "name": "Romania - Send E-Factura - legacy",
     "version": "18.0.1.0.0",
     "category": "Accounting/Localizations/EDI",
     "summary": "Bridge module for sending Romanian E-Factura to the SPV",
     "countries": ["ro"],
-    "website": "https://github.com/OCA/l10n-romania",
+    "website": "https://www.terrabit.ro",
     "depends": ["l10n_ro_edi"],
     "data": [],
     "installable": True,
