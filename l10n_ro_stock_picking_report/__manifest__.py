7b5a1bd4727d8d5d27017ba2ebaad8a031237815
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2026-01-13 09:36:37 +0200

    [19.0][FIX] l10n_ro_stock_picking

--- l10n_ro_stock_picking_report/__manifest__.py
+++ l10n_ro_stock_picking_report/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Romania - Terrabit - Picking Reports",
     "summary": "Rapoarte: NIR, aviz, bon consum",
     "license": "AGPL-3",
-    "version": "19.0.1.2.6",
+    "version": "19.0.1.2.7",
     "development_status": "Mature",
     "author": "Dorin Hongu,Terrabit,Odoo Community Association (OCA)",
     "website": "https://www.terrabit.ro",
