22dc90d83ca794e9f2f7151ebab38ef5234da070
danila12 <danila@terrabit.ro>
2026-01-14 12:56:31 +0200

    Add system parameter fallback for reference date in balance confirmation reports, added tests

--- l10n_ro_balance_confirmation/readme/DESCRIPTION.md
+++ l10n_ro_balance_confirmation/readme/DESCRIPTION.md
@@ -44,6 +44,7 @@ The module extends standard Odoo functionality for partners by:
 
 - Extending the `res.partner` model for correct calculation of balances at a specified date
 - Implementing a wizard for entering the reference date
+- If a date is not provided in the wizard, the system parameter "l10n_ro_balance_confirmation.date_to" can be used. Date must be in YYYY-MM-DD (ex. 2026-12-31) format.
 - Developing a customized QWeb report template for the account statement
 - Using Odoo context to pass parameters between components
 
--- l10n_ro_balance_confirmation/report/res_partner.py
+++ l10n_ro_balance_confirmation/report/res_partner.py
@@ -3,6 +3,7 @@
 
 
 from odoo import api, fields, models
+from odoo.tools import safe_eval
 
 
 class ReportPartnerBalance(models.AbstractModel):
@@ -14,10 +15,19 @@ class ReportPartnerBalance(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         if not docids:
             docids = self.env.context.get("active_ids")
+        if not data:
+            data = {}
+        date_to = data.get("date_to") or self.env.context.get("date_to")
+        if not date_to:
+            date_to = self.env["ir.config_parameter"].sudo().get_param("l10n_ro_balance_confirmation.date_to")
+
+            if date_to and isinstance(date_to, str):
+                date_to = fields.Date.to_date(date_to)
+
         return {
             "doc_ids": docids,
             "doc_model": "res.partner",
             "data": data,
             "docs": self.env["res.partner"].browse(docids),
-            "date_to": data.get("date_to", fields.Date.today()),
+            "date_to": date_to or fields.Date.today(),
         }
