f0e5c7629d68d05e7ecb1324e461f6fa47cb521b
danila12 <danila@terrabit.ro>
2026-01-16 12:04:38 +0200

    add email template and enhance date handling

--- l10n_ro_balance_confirmation/__manifest__.py
+++ l10n_ro_balance_confirmation/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Romania - Balance confirmation",
-    "version": "17.0.0.0.8",
+    "version": "17.0.0.0.10",
     "category": "Localization",
     "countries": ["ro"],
     "summary": "Generate balance confirmation for partners",
@@ -17,6 +17,7 @@
         "wizard/confirm_balance.xml",
         "security/groups.xml",
         "security/ir.model.access.csv",
+        "data/mail_templates.xml",
     ],
     "maintainers": ["danila12"],
 }
