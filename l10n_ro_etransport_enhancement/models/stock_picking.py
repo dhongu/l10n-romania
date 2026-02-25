3a57009649e79203736dcbfe3c18246b788ba8e9
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2026-02-25 14:52:23 +0200

    modificari din 17

--- l10n_ro_etransport_enhancement/models/stock_picking.py
+++ l10n_ro_etransport_enhancement/models/stock_picking.py
@@ -8,6 +8,7 @@ import pytz
 
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
+from odoo.tools import float_is_zero
 
 
 class Picking(models.Model):
@@ -18,6 +19,8 @@ class Picking(models.Model):
     l10n_ro_shipping_weight_lines = fields.One2many(
         "l10n.ro.stock.picking.weight.line", "picking_id", string="Shipping Weight Lines"
     )
+    total_net_weight = fields.Float()
+    total_gross_weight = fields.Float()
 
     def _compute_l10n_ro_edi_stock_enable(self):
         res = super()._compute_l10n_ro_edi_stock_enable()
@@ -38,8 +41,35 @@ class Picking(models.Model):
 
     @api.model
     def _l10n_ro_edi_stock_get_template_data(self, data: dict):
-        for move in self.move_ids:
-            move._cal_move_weight()
+        # get prices if configured in settings
+        def _get_unit_price_for_uit(move):
+            direction = move.picking_code
+            if direction == "incoming" and move.purchase_line_id:
+                price = (
+                    move.purchase_line_id.price_subtotal / move.purchase_line_id.product_qty
+                    if move.purchase_line_id.product_qty
+                    else 0.00
+                )
+                if move.purchase_line_id.currency_id != move.picking_id.company_id.currency_id:
+                    price = move.purchase_line_id.currency_id._convert(
+                        price,
+                        move.picking_id.company_id.currency_id,
+                        move.picking_id.company_id,
+                        move.picking_id.scheduled_date,
+                    )
+                return price
+            elif direction == "outgoing" and move.sale_line_id:
+                price = move.sale_line_id.price_reduce_taxexcl
+                if move.sale_line_id.currency_id != move.picking_id.company_id.currency_id:
+                    price = move.sale_line_id.currency_id._convert(
+                        price,
+                        move.picking_id.company_id.currency_id,
+                        move.picking_id.company_id,
+                        move.picking_id.scheduled_date,
+                    )
+                return price
+            return 0.00
+
         res = super()._l10n_ro_edi_stock_get_template_data(data)
         for key in ("locStartTraseuRutier", "locFinalTraseuRutier"):
             locatie = res["data"]["notificare"][key].get("locatie", {})
@@ -67,35 +97,38 @@ class Picking(models.Model):
             res["data"]["notificare"]["dateTransport"]["dataTransport"] = today
 
         for item in res["data"]["notificare"]["bunuriTransportate"]:
-            # fix bug
+            # fix bug and get price from move if cost price is 0
+            if float_is_zero(item["valoareLeiFaraTva"], precision_rounding=0.01):
+                # try to find the move
+                product_name = item["denumireMarfa"]
+                move = self.env["stock.move"].search(
+                    [("product_id.name", "=", product_name), ("picking_id", "in", self.ids)], limit=1
+                )
+                if move:
+                    price_unit = 0.0
+                    if move.stock_valuation_layer_ids:
+                        quantity = 0.0
+                        value = 0.0
+                        for svl in move.stock_valuation_layer_ids:
+                            quantity += svl.quantity
+                            value += svl.value
+                        if quantity:
+                            price_unit = value / quantity
+                        else:
+                            price_unit = 0.0
+                    if not price_unit:
+                        price_unit = move.product_id.standard_price
+                    if not price_unit:
+                        price_unit = move.product_id.list_price
+                    if not price_unit:
+                        raise UserError(_("Nu am gasit un pret pentru %s") % move.product_id.display_name)
+                    item["valoareLeiFaraTva"] = price_unit
             item["valoareLeiFaraTva"] = round(item["valoareLeiFaraTva"] * item["cantitate"], 2)
             # fix rounding - ex. 0.470000000000003
             item["greutateNeta"] = round(item["greutateNeta"], 2)
             item["greutateBruta"] = round(item["greutateBruta"], 2)
 
-        # get prices if configured in settings
-        def _get_unit_price_for_uit(move):
-            direction = move.picking_code
-            if direction == "incoming" and move.purchase_line_id:
-                price = (
-                    move.purchase_line_id.price_subtotal / move.purchase_line_id.product_qty
-                    if move.purchase_line_id.product_qty
-                    else 0.00
-                )
-                if move.purchase_line_id.currency_id != move.picking_id.company_id.currency_id:
-                    price = move.purchase_line_id.currency_id._convert(
-                        price, move.picking_id.company_id.currency_id, move.picking_id.company_id, move.scheduled_date
-                    )
-                return price
-            elif direction == "outgoing" and move.sale_line_id:
-                price = move.sale_line_id.price_reduce_taxexcl
-                if move.sale_line_id.currency_id != move.picking_id.company_id.currency_id:
-                    price = move.sale_line_id.currency_id._convert(
-                        price, move.picking_id.company_id.currency_id, move.picking_id.company_id, move.scheduled_date
-                    )
-                return price
-            return 0.00
-
+        move_id = False
         if self and self.company_id.l10n_ro_etransport_get_order_value:
             if len(data["stock_move_ids"]) != len(res["data"]["notificare"]["bunuriTransportate"]):
                 raise UserError(_("UIT lines and moves lines are not the same. Cannot get prices."))
@@ -109,6 +142,8 @@ class Picking(models.Model):
                     if move_id:
                         unit_price = _get_unit_price_for_uit(move_id)
                         if unit_price:
+                            if self and self.company_id.l10n_ro_etransport_get_validated_qty:
+                                item["cantitate"] = move_id.quantity
                             item["valoareLeiFaraTva"] = round(unit_price * item["cantitate"], 2)
                         if self.l10n_ro_shipping_weights:
                             weight_line = self.l10n_ro_shipping_weight_lines.filtered(
@@ -136,8 +171,24 @@ class Picking(models.Model):
                     if move_id:
                         unit_price = _get_unit_price_for_uit(move_id)
                         if unit_price:
+                            if move_id.company_id.l10n_ro_etransport_get_validated_qty:
+                                item["cantitate"] = move_id.quantity
                             item["valoareLeiFaraTva"] = round(unit_price * item["cantitate"], 2)
+                        if move_id.picking_id.batch_id and move_id.picking_id.batch_id.l10n_ro_shipping_weights:
+                            weight_line = move_id.picking_id.batch_id.l10n_ro_shipping_weight_lines.filtered(
+                                lambda x, move_id=move_id: x.move_id == move_id
+                            )
+                            if weight_line:
+                                item["greutateNeta"] = round(weight_line.net_weight, 2)
+                                item["greutateBruta"] = round(weight_line.gross_weight, 2)
                     item_no += 1
+        # remove lines with 0 quantity, it's possible when the validated qty is used
+        if move_id and move_id.company_id.l10n_ro_etransport_get_validated_qty:
+            for item in res["data"]["notificare"]["bunuriTransportate"]:
+                if float_is_zero(item["cantitate"], precision_rounding=0.01):
+                    res["data"]["notificare"]["bunuriTransportate"].pop(
+                        res["data"]["notificare"]["bunuriTransportate"].index(item)
+                    )
         return res
 
     def action_l10n_ro_edi_stock_fetch_status(self):
@@ -150,6 +201,7 @@ class Picking(models.Model):
 
     def l10n_ro_compute_weight_lines(self):
         for picking in self:
+            picking.l10n_ro_shipping_weight_lines.unlink()
             vals = []
             for move in picking.move_ids:
                 if move.quantity > 0:
@@ -164,17 +216,34 @@ class Picking(models.Model):
                             .id,
                         }
                     )
-            picking.l10n_ro_shipping_weight_lines.create(vals)
+            if vals:
+                self.env["l10n.ro.stock.picking.weight.line"].create(vals)
 
-    # @api.model
-    # def _l10n_ro_edi_stock_validate_data(self, data: dict):
-    #     errors = super()._l10n_ro_edi_stock_validate_data(data)
-    #
-    #     for error in errors:
-    #         if error == _("The delivery carrier partner has to be located in Romania."):
-    #             errors.remove(error)
-    #
-    #     return errors
+    def l10n_ro_distribute_weights(self):
+        for picking in self:
+            if not picking.l10n_ro_shipping_weight_lines:
+                continue
+            if float_is_zero(picking.total_net_weight, precision_digits=4) or float_is_zero(
+                picking.total_gross_weight, precision_digits=4
+            ):
+                raise UserError(_("Total net and gross weights must be greater than 0."))
+
+            current_net_total = sum(picking.l10n_ro_shipping_weight_lines.mapped("net_weight"))
+            current_gross_total = sum(picking.l10n_ro_shipping_weight_lines.mapped("gross_weight"))
+            net_diff = picking.total_net_weight - current_net_total
+            gross_diff = picking.total_gross_weight - current_gross_total
+
+            if not float_is_zero(net_diff, precision_digits=4) or not float_is_zero(gross_diff, precision_digits=4):
+                for line in picking.l10n_ro_shipping_weight_lines:
+                    if not float_is_zero(current_net_total, precision_digits=4):
+                        line.net_weight += net_diff * (line.net_weight / current_net_total)
+                    else:
+                        line.net_weight = picking.total_net_weight / len(picking.l10n_ro_shipping_weight_lines)
+
+                    if not float_is_zero(current_gross_total, precision_digits=4):
+                        line.gross_weight += gross_diff * (line.gross_weight / current_gross_total)
+                    else:
+                        line.gross_weight = picking.total_gross_weight / len(picking.l10n_ro_shipping_weight_lines)
 
     @api.model
     def _l10n_ro_edi_stock_validate_data(self, data: dict):
