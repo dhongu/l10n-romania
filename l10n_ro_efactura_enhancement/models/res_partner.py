dd7e017b9e989e6d4176261a0f855cf5dc59304b
Dorin Hongu <dhongu@gmail.com>
2026-03-10 21:17:47 +0200

    Actualizează versiunea și extinde funcționalitatea pentru parteneri
    Incrementarea versiunii modulului la `18.0.0.1.13` pentru a reflecta adăugarea logicii `_get_suggested_invoice_edi_format` în `res.partner`. Această metodă prioritizează formatul CIUS RO pentru partenerii din România.

---
+++ l10n_ro_efactura_enhancement/models/res_partner.py
@@ -0,0 +1,17 @@
+
+
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+
+    def _get_suggested_invoice_edi_format(self):
+        # EXTENDS 'account'
+        res = super()._get_suggested_invoice_edi_format()
+        if self.country_code == 'RO':
+            return 'ciusro'
+        else:
+            return res
