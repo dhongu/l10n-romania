427215878364a44bef51ad7a3c53e2709cf0fdaa
Dorin Hongu <dhongu@gmail.com>
2026-03-12 21:26:46 +0200

    Adaugă modulul „l10n_ro_stock_age_report” pentru raportarea stocurilor învechite
    Acest modul permite generarea unui raport privind vechimea stocurilor, incluzând funcționalități avansate precum intervale configurabile de vechime, afișarea valorilor și cantităților în intervale de timp definite și integrarea informațiilor contabile. Include, de asemenea, vizualizări pivot și listă pentru analiza detaliată.

---
+++ l10n_ro_stock_age_report/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2022 NextERP Romania
+# Copyright (C) 2022 Saai SOFT
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Romania - Stock Aged Report",
+    "version": "18.0.0.0.2",
+    "category": "Localization",
+    "summary": "Romania - Stock Aged Report",
+    "author": "NextERP Romania," "Dakai Soft," "Terrabit," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "depends": ["l10n_ro_stock", "l10n_ro_stock_account"],
+    "license": "AGPL-3",
+    "data": [
+        "wizard/stock_age_report.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "post_init_hook": "post_init_hook",
+    "maintainers": ["feketemihai", "mcojocaru", "adrian-dks", "dhongu"],
+}
