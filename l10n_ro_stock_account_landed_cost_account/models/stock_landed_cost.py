b141d77d249f6fac26911bd45f96c4ab24410a98
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2026-03-17 11:22:56 +0200

    [18.0][FIX] l10m_ro_stock_account_landed_cost_account

--- l10n_ro_stock_account_landed_cost_account/models/stock_landed_cost.py
+++ l10n_ro_stock_account_landed_cost_account/models/stock_landed_cost.py
@@ -26,9 +26,11 @@ class AdjustmentLines(models.Model):
 
         fiscal_position = self.cost_id.account_journal_id.l10n_ro_fiscal_position_id
         if fiscal_position:
-            credit_account_id = fiscal_position.map_account(credit_account_id)
-            debit_account_id = fiscal_position.map_account(debit_account_id)
-            already_out_account_id = fiscal_position.map_account(already_out_account_id)
+            credit_account_id = fiscal_position.map_account(self.env["acount.acount"].browse(credit_account_id)).id
+            debit_account_id = fiscal_position.map_account(self.env["acount.acount"].browse(debit_account_id)).id
+            already_out_account_id = fiscal_position.map_account(
+                self.env["acount.acount"].browse(already_out_account_id)
+            ).id
 
         return super()._create_account_move_line(
             move, credit_account_id, debit_account_id, qty_out, already_out_account_id
