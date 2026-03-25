583fa62dd08e844e4a9e2b8e020d1e78e12be200
danila12 <danila@terrabit.ro>
2026-03-25 13:02:18 +0200

    fix `cbc:InvoiceTypeCode` if missing in invoice XML handling

--- l10n_ro_edi_ubl_sale_store/__manifest__.py
+++ l10n_ro_edi_ubl_sale_store/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Deltatech Sale from Store UBL",
-    "version": "18.0.1.0.5",
+    "version": "18.0.1.0.6",
     "author": "Terrabit, Dorin Hongu, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "summary": "Sale from store",
