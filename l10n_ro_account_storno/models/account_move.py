f209114ec410fce5d255b8b01148f3c0091f88b9
Dorin Hongu <dhongu@gmail.com>
2026-03-31 13:06:35 +0300

    Comentează codul legat de logica storno neutilizată
    Elimină utilizarea activă a logicii storno din `account_move.py` prin transformarea acesteia în comentarii. Modificarea ajută la păstrarea codului pentru referință viitoare, fără a-l afecta funcțional.

--- l10n_ro_account_storno/models/account_move.py
+++ l10n_ro_account_storno/models/account_move.py
@@ -17,30 +17,30 @@ class AccountMove(models.Model):
         return res
 
 
-class AccountMoveLine(models.Model):
-    _inherit = "account.move.line"
-
-    storno_line = fields.Boolean()
-
-    def _compute_debit_credit(self):
-        res = super()._compute_debit_credit()
-        for line in self:
-            if line.company_id.account_storno:
-                if line.storno_line:
-                    line.debit = -line.balance if line.balance > 0.0 else 0.0
-                    line.credit = line.balance if line.balance < 0.0 else 0.0
-
-                # if line.account_id.l10n_ro_usage == "activ" and line.credit:
-                #     line.debit = -line.credit
-                #     line.credit = 0.0
-                # if line.account_id.l10n_ro_usage == "pasiv" and line.debit:
-                #     line.credit = -line.debit
-                #     line.debit = 0
-                if line.credit < 0.0 or line.debit < 0.0:
-                    line.storno_line = True
-        return res
-
-    def _sanitize_vals(self, vals):
-        if vals.get("storno_line"):
-            return vals
-        return super()._sanitize_vals(vals)
+# class AccountMoveLine(models.Model):
+#     _inherit = "account.move.line"
+#
+#     storno_line = fields.Boolean()
+#
+#     def _compute_debit_credit(self):
+#         res = super()._compute_debit_credit()
+#         for line in self:
+#             if line.company_id.account_storno:
+#                 if line.storno_line:
+#                     line.debit = -line.balance if line.balance > 0.0 else 0.0
+#                     line.credit = line.balance if line.balance < 0.0 else 0.0
+#
+#                 # if line.account_id.l10n_ro_usage == "activ" and line.credit:
+#                 #     line.debit = -line.credit
+#                 #     line.credit = 0.0
+#                 # if line.account_id.l10n_ro_usage == "pasiv" and line.debit:
+#                 #     line.credit = -line.debit
+#                 #     line.debit = 0
+#                 if line.credit < 0.0 or line.debit < 0.0:
+#                     line.storno_line = True
+#         return res
+#
+#     def _sanitize_vals(self, vals):
+#         if vals.get("storno_line"):
+#             return vals
+#         return super()._sanitize_vals(vals)
