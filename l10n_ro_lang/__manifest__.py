64c070e98a58e16cb5ad28caae9a713f3c2313af
Dorin Hongu <dhongu@gmail.com>
2026-04-10 06:33:15 +0300

    [IMP] l10n_ro_lang: actualizează manifestul modulului
    Schimbă versiunea modulului la 19.0.0.0.2 și actualizează atributul "author" pentru a include Terrabit. Adaugă un nou atribut `development_status` cu valoarea "Mature", reflectând starea actuală a dezvoltării.

--- l10n_ro_lang/__manifest__.py
+++ l10n_ro_lang/__manifest__.py
@@ -1,11 +1,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Romania - Set Lang to Romanian",
-    "version": "18.0.0.0.2",
+    "version": "19.0.0.0.2",
     "category": "Localization",
     "countries": ["ro"],
     "summary": "Set language to romanian if IP is from Romania",
-    "author": "Dorin Hongu,Odoo Community Association (OCA)",
+    "author": "Terrabit,Dorin Hongu,Odoo Community Association (OCA)",
     "website": "https://www.terrabit.ro",
     "depends": [
         "website",
@@ -13,4 +13,5 @@
     "license": "AGPL-3",
     "data": [],
     "maintainers": ["dhongu"],
+    "development_status": "Mature",
 }
