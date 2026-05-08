c0a76a40134dc4ef7636c37f090f9543e5426b05
Dorin Hongu <dhongu@gmail.com>
2026-05-08 17:30:58 +0300

    Actualizează versiunea modulului Purchase Message SPV
    Incrementarea versiunii de la 18.0.0.0.1 la 19.0.0.0.1 pentru a asigura compatibilitatea cu noua versiune Odoo. Această modificare reflectă evoluția și stabilitatea funcționalităților modulului.

--- l10n_ro_message_spv_purchase/__manifest__.py
+++ l10n_ro_message_spv_purchase/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Purchase Message SPV",
-    "version": "18.0.0.0.1",
+    "version": "19.0.0.0.1",
     "author": "Terrabit, Dorin Hongu, Odoo Community Association (OCA)",
     "website": "https://www.terrabit.ro",
     "summary": "Add SPV message on purchase orders for Romania",
