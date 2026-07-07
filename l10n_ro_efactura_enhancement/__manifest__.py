59117da49459814bdf0a331a529917852b24eadb
danila12 <danila@terrabit.ro>
2026-07-07 16:24:24 +0300

    fix test mode

--- l10n_ro_efactura_enhancement/__manifest__.py
+++ l10n_ro_efactura_enhancement/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "eFactura Enhancement",
-    "version": "19.0.0.3.18",
+    "version": "19.0.0.3.19",
     "author": "Terrabit, Dorin Hongu, Odoo Community Association (OCA)",
     "website": "https://www.terrabit.ro",
     "summary": "eFactura Enhancement",
